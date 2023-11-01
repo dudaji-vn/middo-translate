706ed8674718eba5e16c868dd1e64d0bff33c77a
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-01 09:32:42 +0700

    Test cloud translate api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,46 +11,48 @@ interface HomeProps {
 }
 
 export default async function Home(props: HomeProps) {
-	// const sourceText = props.searchParams.query || '';
-	// const sourceLanguage =
-	// 	props.searchParams.sourceLanguage || DEFAULT_LANGUAGES_CODE.VN;
-	// const targetLanguage =
-	// 	props.searchParams.targetLanguage || DEFAULT_LANGUAGES_CODE.KR;
-	// const targetResult = await translateText(
-	// 	sourceText,
-	// 	sourceLanguage,
-	// 	targetLanguage,
-	// );
-	// const sourceEnglishResult = await translateText(
-	// 	sourceText,
-	// 	sourceLanguage,
-	// 	DEFAULT_LANGUAGES_CODE.EN,
-	// );
-	// const targetEnglishResult = await translateText(
-	// 	targetResult,
-	// 	targetLanguage,
-	// 	DEFAULT_LANGUAGES_CODE.EN,
-	// );
-	// const supportLanguages = await getSupportLanguages();
+	const sourceText = props.searchParams.query || '';
+	const sourceLanguage =
+		props.searchParams.sourceLanguage || DEFAULT_LANGUAGES_CODE.VN;
+	const targetLanguage =
+		props.searchParams.targetLanguage || DEFAULT_LANGUAGES_CODE.KR;
+	const targetResult = await translateText(
+		sourceText,
+		sourceLanguage,
+		targetLanguage,
+	);
+	const sourceEnglishResult = await translateText(
+		sourceText,
+		sourceLanguage,
+		DEFAULT_LANGUAGES_CODE.EN,
+	);
+	const targetEnglishResult = await translateText(
+		targetResult,
+		targetLanguage,
+		DEFAULT_LANGUAGES_CODE.EN,
+	);
+	const supportLanguages = await getSupportLanguages();
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24">
-			{/* {supportLanguages.map((language) => (
-				<div
-					key={language.code}
-					className="flex flex-col items-center justify-center"
-				>
-					<h1 className="text-2xl">{language.name}</h1>
-					<p className="text-2xl">{language.code}</p>
-				</div>
-			))} */}
+			<div className="max-h-24 overflow-scroll mb-10">
+				{supportLanguages.map((language) => (
+					<div
+						key={language.code}
+						className="flex flex-col items-center justify-center"
+					>
+						<h1 className="text-2xl">{language.name}</h1>
+						<p className="text-2xl">{language.code}</p>
+					</div>
+				))}
+			</div>
 			<TranslateEditor />
-			{/* <div>
+			<div>
 				<span>{sourceEnglishResult}</span>
 			</div>
 			<TranslateResult
 				result={targetResult}
 				resultEnglish={targetEnglishResult}
-			/> */}
+			/>
 		</main>
 	);
 }
