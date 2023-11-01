3517d4bdcdd2a801b8673f8c8a74843ad9dde6a2
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-01 08:31:16 +0700

    Add env configuration file for cloud translate

---
+++ src/configs/env.ts
@@ -0,0 +1,8 @@
+export const GOOGLE_CLOUD_TYPE = process.env.TYPE as string;
+export const GOOGLE_CLOUD_PROJECT_ID = process.env.PROJECT_ID as string;
+export const GOOGLE_CLOUD_PRIVATE_KEY = process.env.PRIVATE_KEY!.replace(
+	/\\n/g,
+	'\n',
+) as string;
+export const GOOGLE_CLOUD_CLIENT_EMAIL = process.env.CLIENT_EMAIL as string;
+export const GOOGLE_CLOUD_CLIENT_ID = process.env.CLIENT_ID as string;
