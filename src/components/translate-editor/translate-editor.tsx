404052a3dcd34fff5c77afef072f8ef6e9fd5745
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-03 16:47:08 +0700

    Add new components and styles for translation editor

--- src/components/translate-editor/translate-editor.tsx
+++ src/components/translate-editor/translate-editor.tsx
@@ -1,30 +1,101 @@
 'use client';
+
+import './style.css';
+
+import { ChangeEvent, useEffect, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
 
-export interface TranslateEditorProps {}
+import { CloseCircleOutline } from '@easy-eva-icons/react';
+import { TranslateEditorWrapper } from './translate-editor-wrapper';
+import { cn } from '@/utils/cn';
+import { detectLanguage } from '@/services/languages';
+import { useAdjustTextStyle } from '@/hooks/use-adjust-text-style';
+import { useDebounce } from 'usehooks-ts';
+import { useTextAreaResize } from '@/hooks/use-text-area-resize';
 
-export const TranslateEditor = (props: TranslateEditorProps) => {
-	const searchParams = useSearchParams();
-	const pathname = usePathname();
-	const { replace } = useRouter();
-	const handleInput = useDebouncedCallback((term) => {
-		const params = new URLSearchParams(searchParams);
-		if (term) {
-			params.set('query', term);
-		} else {
-			params.delete('query');
-		}
-		replace(`${pathname}?${params.toString()}`);
-	}, 300);
-	return (
-		<div>
-			<input
-				onChange={(e) => handleInput(e.target.value)}
-				className="bg-gray-100 border-2 border-gray-300 focus:outline-none focus:border-blue-500 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-				type="text"
-				placeholder="hello"
-			/>
-		</div>
-	);
+export interface TranslateEditorProps {
+  className?: string;
+  languageCode?: string;
+  disabled?: boolean;
+  sourceTranslateResult?: string;
+}
+
+export const TranslateEditor = ({
+  className,
+  languageCode = 'auto',
+  disabled = false,
+  sourceTranslateResult,
+}: TranslateEditorProps) => {
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const searchParams = useSearchParams();
+  const text = searchParams.get('query') || '';
+  const [value, setValue] = useState<string>(text);
+  const textStyles = useAdjustTextStyle(value);
+  const debouncedValue = useDebounce<string>(value, 300);
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    if (!isFocus || value === sourceTranslateResult) {
+      return;
+    }
+    if (debouncedValue) {
+      params.set('query', debouncedValue);
+      params.delete('edit');
+      params.delete('mquery');
+      if (!languageCode || languageCode === 'auto') {
+        detectLanguage(debouncedValue).then((res) => {
+          params.set('source', res);
+
+          replace(`${pathname}?${params.toString()}`);
+        });
+      }
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue, isFocus]);
+
+  const { textAreaRef } = useTextAreaResize(value);
+
+  useEffect(() => {
+    if (sourceTranslateResult && !text) {
+      setValue(sourceTranslateResult);
+    }
+  }, [sourceTranslateResult, text]);
+
+  return (
+    <TranslateEditorWrapper
+      languageCode={languageCode}
+      type={disabled ? 'result' : 'default'}
+      className={className}
+    >
+      <button
+        onClick={() => {
+          setValue('');
+        }}
+        className="btn-icon absolute right-3 top-3"
+      >
+        <CloseCircleOutline className="h-6 w-6 opacity-60" />
+      </button>
+
+      <textarea
+        rows={1}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        disabled={disabled}
+        ref={textAreaRef}
+        value={value}
+        onChange={handleChange}
+        className={cn('inputTranslate transition-all', textStyles)}
+        placeholder="hello"
+      />
+    </TranslateEditorWrapper>
+  );
 };
