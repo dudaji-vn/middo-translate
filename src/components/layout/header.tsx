600dc869cce469ca3758132a3fd31cab15ee89f1
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-03 07:50:35 +0700

    Refactor layout component to use MainLayout instead of Header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,7 +10,7 @@ export const Header = (props: Props) => {
   return (
     <div className="flex items-center justify-between p-5">
       <h1 className="text-3xl font-bold">{PUBLIC_APP_NAME}</h1>
-      <IconButton variant="secondary" size="sm">
+      <IconButton variant="secondary" size="icon" className="rounded-xl p-10">
         <Menu />
       </IconButton>
     </div>
