b39cc56c4a24f3d59d3e711e6d01d50a15f519f7
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-07 13:37:53 +0700

    Add TranslateMiddleEditor and CompareBar components.

--- src/components/translate-editor/index.ts
+++ src/components/translate-editor/index.ts
@@ -1,6 +1,9 @@
 export { TranslateEditor } from './translate-editor';
 export { TranslateMiddle } from './translate-middle';
 export { TranslateResult } from './translate-result';
+export { TranslateMiddleEditor } from './translate-middle-editor';
+export { CompareBar } from './compare-bar';
 export type { TranslateResultProps } from './translate-result';
 export type { TranslateEditorProps } from './translate-editor';
 export type { TranslateMiddleProps } from './translate-middle';
+export type { CompareBarProps } from './compare-bar';
