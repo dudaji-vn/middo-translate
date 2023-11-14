e4a8919975cb9fa6a5d8cb2d2ea95ccabc0bf423
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-14 17:18:28 +0700

    Add join page for online conversation feature

---
+++ src/app/(online-conversation)/online-conversation/join/[code]/page.tsx
@@ -0,0 +1,80 @@
+import './style.css';
+
+import {
+  RoomJoinerProvider,
+  RoomJoinerSubmit,
+  SelectNativeLanguage,
+  UsernameInput,
+} from '@/components/online-conversation/join';
+
+import { getConversation } from '@/services/conversation';
+
+interface CreateProps {
+  params: {
+    code: string;
+  };
+}
+
+export default async function Create(props: CreateProps) {
+  const room = await getConversation(props.params.code);
+  return (
+    <RoomJoinerProvider room={room}>
+      <div className="myContainer">
+        <div className="wrapper">
+          <div className="columnWrapper">
+            <div className="rightColumn">
+              <img className="introImg" src="/conversation_intro.png" alt="" />
+            </div>
+            <div className="leftColumn">
+              <div className="conversationForm">
+                <FormTitle>Join conversation</FormTitle>
+                <div className="formSection mb-0">
+                  <FormField label="     ">
+                    <UsernameInput />
+                  </FormField>
+                  <FormField label="Language">
+                    <SelectNativeLanguage />
+                  </FormField>
+                </div>
+                <RoomJoinerSubmit />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RoomJoinerProvider>
+  );
+}
+
+const FormTitle = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="CPtitle">
+      <div
+        style={{
+          width: '4px',
+          height: '24px',
+          borderRadius: '99px',
+        }}
+        className="bg-primary"
+      ></div>
+      <h4>{children}</h4>
+    </div>
+  );
+};
+
+const FormField = ({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="formField">
+      <label className="label" htmlFor="Name">
+        {label}
+      </label>
+      {children}
+    </div>
+  );
+};
