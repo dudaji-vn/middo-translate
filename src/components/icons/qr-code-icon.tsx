3b7c298ce1c79cd2fe9830fe6cdf019f7fd8978f
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-14 17:20:31 +0700

    Add QR code and spinner icons

---
+++ src/components/icons/qr-code-icon.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import { SVGProps } from 'react';
+
+export const QRCodeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={28}
+    height={28}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M3 7a4 4 0 0 1 4-4h4a1.175 1.175 0 0 1 0 2.35H7.35a2 2 0 0 0-2 2V11A1.175 1.175 0 0 1 3 11V7ZM25 21a4 4 0 0 1-4 4h-4a1.175 1.175 0 0 1 0-2.35h3.65a2 2 0 0 0 2-2V17A1.175 1.175 0 0 1 25 17v4ZM7 25a4 4 0 0 1-4-4v-4a1.175 1.175 0 0 1 2.35 0v3.65a2 2 0 0 0 2 2H11A1.175 1.175 0 0 1 11 25H7ZM21 3a4 4 0 0 1 4 4v4a1.175 1.175 0 0 1-2.35 0V7.35a2 2 0 0 0-2-2H17A1.175 1.175 0 0 1 17 3h4Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M9.35 9.35v1.3h1.3v-1.3h-1.3ZM9 7a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2H9ZM9.35 17.35v1.3h1.3v-1.3h-1.3ZM9 15a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2H9Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="currentColor"
+      d="M15 16a1 1 0 0 1 1-1h.35a1 1 0 0 1 1 1v.35a1 1 0 0 1-1 1H16a1 1 0 0 1-1-1V16ZM15 19.65a1 1 0 0 1 1-1h.35a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1H16a1 1 0 0 1-1-1v-.35Z"
+    />
+    <path
+      fill="currentColor"
+      d="M16.825 17.825a1 1 0 0 1 1-1h.35a1 1 0 0 1 1 1v.35a1 1 0 0 1-1 1h-.35a1 1 0 0 1-1-1v-.35Z"
+    />
+    <path
+      fill="currentColor"
+      d="M18.65 16a1 1 0 0 1 1-1H20a1 1 0 0 1 1 1v.35a1 1 0 0 1-1 1h-.35a1 1 0 0 1-1-1V16ZM18.65 19.65a1 1 0 0 1 1-1H20a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1h-.35a1 1 0 0 1-1-1v-.35Z"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M17.35 9.35v1.3h1.3v-1.3h-1.3ZM17 7a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
