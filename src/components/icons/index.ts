0009386825b25bc31395e8ebe8cc96e4e9eb24a4
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-17 10:13:17 +0700

    Add SvgSpinners270RingWithBg icon and loading animation to InputEditor

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -6,3 +6,4 @@ export * from './copy-img-icon';
 export * from './multiple-copy-icon';
 export * from './svg-spinners-gooey-balls2';
 export * from './qr-code-icon';
+export * from './SvgSpinners270RingWithBg';
