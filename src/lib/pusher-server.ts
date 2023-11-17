8cfef1b10dbeea51faf55e7a5ff10735c0f9649e
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-17 10:10:53 +0700

    Add Pusher client and server configurations.

--- src/lib/pusher-server.ts
+++ src/lib/pusher-server.ts
@@ -4,17 +4,12 @@ import {
 } from '@/configs/env.public';
 import { PUSHER_APP_ID, PUSHER_SECRET } from '@/configs/env.private';
 
-import PusherClient from 'pusher-js';
 import PusherServer from 'pusher';
 
-export const pusherClient = new PusherClient('7e4a4fbde16ab3fbffb7', {
-  cluster: 'ap1',
-});
-
 export const pusherServer = new PusherServer({
-  appId: '1708592',
-  key: '7e4a4fbde16ab3fbffb7',
-  secret: 'f9ec0bb4d377e18b5012',
-  cluster: 'ap1',
+  appId: PUSHER_APP_ID,
+  key: NEXT_PUBLIC_PUSHER_KEY,
+  secret: PUSHER_SECRET,
+  cluster: NEXT_PUBLIC_PUSHER_CLUSTER,
   useTLS: true,
 });
