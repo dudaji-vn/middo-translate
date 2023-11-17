dafa2f6cba4d148efb2ec45b353d5f95d795b4ce
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-17 10:11:32 +0700

    Add real-time member join/leave functionality to side chat

--- src/components/online-conversation/chat/side-chat/side-chat.tsx
+++ src/components/online-conversation/chat/side-chat/side-chat.tsx
@@ -1,15 +1,44 @@
+'use client';
+
+import { ArrowForward } from '@easy-eva-icons/react';
+import { IconButton } from '@/components/button';
 import { SideChatBody } from './side-chat-body';
 import { SideChatFooter } from './side-footer';
 import { SideChatHeader } from './side-chat-header';
+import { cn } from '@/utils/cn';
+import { useChat } from '../chat-context';
+import { useIsMobile } from '@/hooks/use-is-mobile';
 
 export interface SideChatProps {}
 
 export const SideChat = (props: SideChatProps) => {
+  const { room, user, closeSideChat, showSideChat } = useChat();
+  const isMobile = useIsMobile();
   return (
-    <div className="flex min-w-[30vw] flex-col gap-3 bg-background-darker">
-      <SideChatHeader code="ahls" />
-      <SideChatBody />
-      <SideChatFooter />
-    </div>
+    <>
+      {showSideChat && isMobile && (
+        <div className="fixed bottom-0 left-0 right-0 top-0 bg-black/70"></div>
+      )}
+      <div
+        className={cn(
+          'flex h-full min-w-[30vw] flex-col gap-3 overflow-y-auto bg-background-darker',
+          isMobile
+            ? 'fixed bottom-0 right-0 top-0 w-[80vw] transition-all duration-300'
+            : '',
+          showSideChat ? 'translate-x-0' : 'translate-x-full',
+        )}
+      >
+        {isMobile && (
+          <div className="mt-2 flex justify-end px-[5vw]">
+            <IconButton onClick={closeSideChat} variant="ghost">
+              <ArrowForward />
+            </IconButton>
+          </div>
+        )}
+        <SideChatHeader code={room.code} />
+        <SideChatBody room={room} />
+        <SideChatFooter roomCode={room.code} user={user} />
+      </div>
+    </>
   );
 };
--- src/components/online-conversation/chat/side-chat/side-footer.tsx
+++ src/components/online-conversation/chat/side-chat/side-footer.tsx
@@ -1,7 +1,34 @@
-import { MemberList } from './member-list';
+'use client';
 
-export interface SideChatFooterProps {}
+import { Participant } from '@/types/room';
+import { ROUTE_NAMES } from '@/configs/route-name';
+import { leaveConversation } from '@/services/conversation';
+import { useRouter } from 'next/navigation';
 
-export const SideChatFooter = (props: SideChatFooterProps) => {
-  return <div className=""></div>;
+export interface SideChatFooterProps {
+  roomCode: string;
+  user: Participant;
+}
+
+export const SideChatFooter = ({ roomCode, user }: SideChatFooterProps) => {
+  const router = useRouter();
+  const handleLeaveRoom = async () => {
+    router.push(ROUTE_NAMES.ONLINE_CONVERSATION);
+  };
+  return (
+    <div className="">
+      <button
+        onClick={handleLeaveRoom}
+        className="w-full bg-background py-4 text-center font-medium text-error"
+      >
+        Leave room
+      </button>
+      <div className="mt-10 flex items-center justify-center">
+        <div className="">
+          <p className="mb-2 font-light ">Power by</p>
+          <img src="/logo.png" alt="qr-code" width={150} />
+        </div>
+      </div>
+    </div>
+  );
 };
