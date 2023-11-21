f90c1a29bc9f112c0bbc5b1c0a9fd712a8cf6e9e
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-21 16:28:21 +0700

    Fix toast duration in useTextCopy hook

--- src/hooks/use-text-copy.ts
+++ src/hooks/use-text-copy.ts
@@ -5,7 +5,7 @@ export const useTextCopy = (_text?: string) => {
   const copy = (text?: string) => {
     const textToCopy = text || _text || '';
     navigator.clipboard.writeText(textToCopy).then(() => {
-      toast({ description: 'Text copied!' });
+      toast({ description: 'Text copied!', duration: 1000 });
     });
   };
   return { copy };
