e144aeaa23ec151aa71b154e7960d9980ae092df
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-22 15:33:39 +0700

    Refactor message component to show time and handle click event

--- src/components/online-conversation/chat/message.tsx
+++ src/components/online-conversation/chat/message.tsx
@@ -1,8 +1,11 @@
+'use client';
+
+import { forwardRef, useEffect, useState } from 'react';
+
 import { Message as MessageType } from '@/types/room';
 import { Text } from '@/components/text';
 import { TriangleSmall } from '@/components/icons/triangle-small';
 import { cn } from '@/utils/cn';
-import { forwardRef } from 'react';
 import moment from 'moment';
 
 export interface MessageProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -10,40 +13,70 @@ export interface MessageProps extends React.HTMLAttributes<HTMLDivElement> {
   message: MessageType;
   useTranslate?: boolean;
   isShowFull?: boolean;
+  alwaysShowTime?: boolean;
 }
 
 export const Message = forwardRef<HTMLDivElement, MessageProps>(
   (
-    { isMe, message, useTranslate = false, isShowFull = false, ...props },
+    {
+      isMe,
+      message,
+      useTranslate = false,
+      isShowFull = false,
+      alwaysShowTime = false,
+      ...props
+    },
     ref,
   ) => {
+    const [isShowTime, setIsShowTime] = useState(alwaysShowTime);
+    useEffect(() => {
+      setIsShowTime(alwaysShowTime);
+    }, [alwaysShowTime]);
+
     return (
-      <div className={isMe ? 'receiver' : 'sender'}>
-        <div className="cMessageWrapper">
-          <div className="cMessage">
-            <Text
-              value={
-                useTranslate ? message.translatedContent || '' : message.content
-              }
-            />
-            {isShowFull && message.englishContent && (
-              <div className="relative mt-2">
-                <TriangleSmall
-                  fill="white"
-                  position="top"
-                  className="absolute left-2 top-0 -translate-y-full"
-                />
-                <div className="mt-2 rounded-lg bg-background p-1 px-2">
-                  <Text
-                    value={message.englishContent}
-                    className="text-sm font-light italic"
+      <>
+        <div
+          onClick={() => {
+            if (alwaysShowTime) return;
+            setIsShowTime(!isShowTime);
+          }}
+          className={cn('cursor-pointer', isMe ? 'receiver' : 'sender')}
+        >
+          <div className="cMessageWrapper">
+            <div className="cMessage">
+              <Text
+                value={
+                  useTranslate
+                    ? message.translatedContent || ''
+                    : message.content
+                }
+              />
+              {isShowFull && message.englishContent && (
+                <div className="relative mt-2">
+                  <TriangleSmall
+                    fill="white"
+                    position="top"
+                    className="absolute left-2 top-0 -translate-y-full"
                   />
+                  <div className="mt-2 rounded-lg bg-background p-1 px-2">
+                    <Text
+                      value={message.englishContent}
+                      className="text-sm font-light italic"
+                    />
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
-      </div>
+        {isShowTime && (
+          <div
+            className={cn('text-xs font-light', isMe ? 'ml-auto pr-3' : 'pl-3')}
+          >
+            Sent on {moment(message.createdAt).format('LT')}
+          </div>
+        )}
+      </>
     );
   },
 );
@@ -52,11 +85,13 @@ export const GroupMessage = ({
   isMe = false,
   useTranslate = false,
   isShowFull = false,
+  isLast = false,
 }: {
   messages: MessageType[];
   isMe?: boolean;
   useTranslate?: boolean;
   isShowFull?: boolean;
+  isLast?: boolean;
 }) => {
   const sender = messages[0].sender;
 
@@ -73,7 +108,9 @@ export const GroupMessage = ({
 
   const bg = `bg-[${sender.color}]`;
   return (
-    <div className={cn('flex w-full items-end gap-3', isMe && 'justify-end')}>
+    <div
+      className={cn('mt-5 flex w-full items-end gap-3 ', isMe && 'justify-end')}
+    >
       {!isMe && (
         <div
           style={{ backgroundColor: sender.color }}
@@ -85,7 +122,7 @@ export const GroupMessage = ({
           {sender.username[0]}
         </div>
       )}
-      <div className="flex flex-col gap-2">
+      <div className="flex max-w-[90%] flex-col gap-2">
         {!isMe && (
           <div className="text-sm font-light">
             {messages[0].sender.username}
@@ -98,13 +135,9 @@ export const GroupMessage = ({
             message={message}
             key={index}
             isMe={isMe}
+            alwaysShowTime={isLast && index === messages.length - 1}
           />
         ))}
-        <div
-          className={cn('text-xs font-light', isMe ? 'ml-auto pr-3' : 'pl-3')}
-        >
-          Sent on {moment(messages[0].createdAt).format('LT')}
-        </div>
       </div>
     </div>
   );
