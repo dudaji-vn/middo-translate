7ed5c5b2a178759ef3fdf2a86dfdcd66a7b0a4bb
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-22 13:08:18 +0700

    Add not-found page and handle room not found scenario

--- src/app/(room-layout)/online-conversation/[code]/page.tsx
+++ src/app/(room-layout)/online-conversation/[code]/page.tsx
@@ -8,6 +8,8 @@ import {
 } from '@/components/online-conversation/chat';
 
 import { ChatProvider } from '@/components/online-conversation/chat/chat-context';
+import Link from 'next/link';
+import { ROUTE_NAMES } from '@/configs/route-name';
 import { getConversation } from '@/services/conversation';
 
 interface RoomConversationProps {
@@ -21,11 +23,24 @@ export default async function RoomConversation({
 }: RoomConversationProps) {
   const room = await getConversation(params.code);
   if (!room) {
-    return <>hello</>;
+    return (
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
+        <img src="/not-found.png" alt="not-found" width={224} />
+        <h3 className="mt-8">Room not found!!!</h3>
+        <p className="mt-3">
+          Please check again or return to homepage to create a new room
+        </p>
+        <Link href={`${ROUTE_NAMES.ONLINE_CONVERSATION}`} className="mt-10">
+          <button className="font-medium text-primary">
+            Return to homepage
+          </button>
+        </Link>
+      </div>
+    );
   }
   return (
-    <ChatProvider room={room}>
-      <div className="chatScreenWrapper h-screen overflow-hidden">
+    <div className="chatScreenWrapper h-screen overflow-hidden">
+      <ChatProvider room={room}>
         <Header />
         <div className="chatElementWrapper flex-1 overflow-hidden">
           <div className="chat">
@@ -34,7 +49,7 @@ export default async function RoomConversation({
           </div>
           <SideChat />
         </div>
-      </div>
-    </ChatProvider>
+      </ChatProvider>
+    </div>
   );
 }
