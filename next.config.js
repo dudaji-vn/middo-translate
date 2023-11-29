e4b939ed8d8f8e324f1a40a3e2dcad798a5e5f52
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-29 07:47:28 +0700

    Add image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'ui-avatars.com',
+      'lh3.googleusercontent.com',
+      'scontent-sea1-1.xx.fbcdn.net',
+      'platform-lookaside.fbsbx.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
