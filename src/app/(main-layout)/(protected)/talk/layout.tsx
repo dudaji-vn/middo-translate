406f0e8e5f6b2cb5d11984081e24f9c90a766646
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-11-30 14:06:51 +0700

    Update ChatLayout component styling

--- src/app/(main-layout)/(protected)/talk/layout.tsx
+++ src/app/(main-layout)/(protected)/talk/layout.tsx
@@ -6,7 +6,7 @@ export interface ChatLayoutProps {
 
 const ChatLayout = ({ children }: ChatLayoutProps) => {
   return (
-    <div className="flex flex-1">
+    <div className="flex h-[calc(100vh_-_90px)] overflow-hidden">
       <div className="hidden min-w-[320px] basis-1/4 sm:block">
         <Inbox />
       </div>
