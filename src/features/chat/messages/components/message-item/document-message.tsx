d67ea67ad576312b23070491d4750a752461de6f
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-07 15:09:23 +0700

    Refactor DocumentMessage component

--- src/features/chat/messages/components/message-item/document-message.tsx
+++ src/features/chat/messages/components/message-item/document-message.tsx
@@ -1,26 +1,34 @@
 import { FileOutline } from '@easy-eva-icons/react';
 import { Media } from '@/types';
+import { cn } from '@/utils/cn';
 import { formatFileSize } from '../../utils';
 
 export interface DocumentProps {
   file: Media;
+  isMe: boolean;
 }
 
-export const DocumentMessage = ({ file }: DocumentProps) => {
+export const DocumentMessage = ({ file, isMe }: DocumentProps) => {
   return (
     <a
       download
       href={file.url}
-      className="flex w-full items-center justify-center gap-2 rounded-lg bg-primary p-2 px-3"
+      className={cn(
+        'flex w-full items-center justify-center gap-2 rounded-lg p-2 px-3',
+        isMe ? 'bg-primary' : 'bg-background-darker',
+      )}
     >
       <div className="rounded-full bg-lighter p-2">
         <FileOutline className="h-5 w-5 text-primary" />
       </div>
       <div className="flex flex-col">
-        <span title={file.name} className="font-semibold text-background">
+        <span
+          title={file.name}
+          className={cn('font-semibold', isMe && 'text-background')}
+        >
           {file.name}
         </span>
-        <span className="text-sm font-light text-background">
+        <span className={cn('text-xs', isMe ? 'text-background' : 'text-text')}>
           {formatFileSize(file.size!)}
         </span>
       </div>
