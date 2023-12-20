a6497d2e253e96dde5940c2017c701f503c84b33
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-20 11:45:35 +0700

    Refactor inbox components

--- src/features/chat/rooms/components/inbox/inbox.tsx
+++ src/features/chat/rooms/components/inbox/inbox.tsx
@@ -9,6 +9,7 @@ import { GroupCreateSide } from '../group-create-side';
 import { InboxMainSide } from './inbox-main-side';
 import { InboxSides } from '../../types';
 import { PrivateCreateSide } from '../private-create-side';
+import { RoomActions } from '../room.actions';
 import { SettingSide } from '../setting-side';
 import { Typography } from '@/components/data-display';
 import { useChangeInboxSide } from '../../hooks/use-change-inbox-side';
@@ -52,37 +53,39 @@ export const Inbox = (props: InboxProps) => {
   }, [side]);
 
   return (
-    <div className="relative flex h-full w-full flex-col overflow-hidden border-r bg-card">
-      <div className="flex-1 overflow-hidden pt-5">
-        <InboxMainSide />
+    <RoomActions>
+      <div className="relative flex h-full w-full flex-col overflow-hidden border-r bg-card">
+        <div className="flex-1 overflow-hidden pt-5">
+          <InboxMainSide />
+        </div>
+        <AnimatePresence>
+          {side !== 'default' && (
+            <motion.div
+              initial={{ x: '100%' }}
+              animate={{ x: 0 }}
+              exit={{ x: '100%' }}
+              transition={{ duration: 0.2 }}
+              className="absolute left-0 top-0 flex h-full w-full flex-col bg-card"
+            >
+              <div className="flex items-center gap-2 px-5 pt-3">
+                <Button.Icon
+                  onClick={changeToDefault}
+                  variant="ghost"
+                  color="default"
+                >
+                  <ArrowBackOutline />
+                </Button.Icon>
+                <Typography variant="default" className="font-semibold">
+                  {sidesMap[side]?.title}
+                </Typography>
+              </div>
+              <div className="flex-1 overflow-hidden">
+                {sidesMap[side]?.component}
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      <AnimatePresence>
-        {side !== 'default' && (
-          <motion.div
-            initial={{ x: '100%' }}
-            animate={{ x: 0 }}
-            exit={{ x: '100%' }}
-            transition={{ duration: 0.2 }}
-            className="absolute left-0 top-0 flex h-full w-full flex-col bg-card"
-          >
-            <div className="flex items-center gap-2 px-5 pt-3">
-              <Button.Icon
-                onClick={changeToDefault}
-                variant="ghost"
-                color="default"
-              >
-                <ArrowBackOutline />
-              </Button.Icon>
-              <Typography variant="default" className="font-semibold">
-                {sidesMap[side]?.title}
-              </Typography>
-            </div>
-            <div className="flex-1 overflow-hidden">
-              {sidesMap[side]?.component}
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+    </RoomActions>
   );
 };
