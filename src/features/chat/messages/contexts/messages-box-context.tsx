88c1aba0d389b23d01fd5482b0d6fd25b37680b1
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-25 16:32:23 +0700

    Fix console.log statements and remove unused imports

--- src/features/chat/messages/contexts/messages-box-context.tsx
+++ src/features/chat/messages/contexts/messages-box-context.tsx
@@ -2,7 +2,6 @@
 
 import { PropsWithChildren, createContext, useContext, useEffect } from 'react';
 
-import { FCMProvider } from '@/providers/fcm.provider';
 import { Message } from '@/features/chat/messages/types';
 import { MessageActions } from '../components/message.actions';
 import { NEXT_PUBLIC_NAME } from '@/configs/env.public';
