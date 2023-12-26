625734660d83aac9b86eae1a5e93e5bf51b25590
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-26 13:01:01 +0700

    Refactor message item components and add useHasFocus hook

--- src/providers/socket.tsx
+++ src/providers/socket.tsx
@@ -3,29 +3,33 @@
 import React, { useEffect } from 'react';
 
 import socket from '@/lib/socket-io';
+import { useAuthStore } from '@/stores/auth';
 
 const SocketProvider = () => {
+  const user = useAuthStore((state) => state.user);
   useEffect(() => {
+    if (!user?._id) {
+      return;
+    }
     socket.connect();
-    // function onConnect() {
-    //   if (user?._id) {
-    //     socket.emit('client.join', user._id);
-    //   }
-    // }
+    function onConnect() {
+      if (user?._id) {
+        socket.emit('client.join', user._id);
+      }
+    }
 
     function onDisconnect() {
       console.log('disconnected');
     }
 
-    // socket.on('connect', onConnect);
+    socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     return () => {
-      // socket.off('connect', onConnect);
+      socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
       socket.disconnect();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [user?._id]);
 
   return <></>;
 };
