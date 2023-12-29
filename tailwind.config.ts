804624b2bf3872e556fc4e7b32f8eb705c1792d8
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-29 15:56:33 +0700

    Fix typo in main-container-height calculation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ module.exports = {
       height: {
         header: 'var(--header-height, 60px)',
 
-        'main-container-height': 'calc(100vh - var(--header-height, 60px))',
+        'main-container-height': 'calc(100dvh - var(--header-height, 60px))',
       },
       colors: {
         border: 'hsl(var(--border))',
