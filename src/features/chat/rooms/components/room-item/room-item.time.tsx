178fcf309eeb6127bbddad40c8a35cc84396c4d8
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-01-02 13:56:52 +0700

    Refactor RoomItemTime component to use HTMLSpanElement

--- src/features/chat/rooms/components/room-item/room-item.time.tsx
+++ src/features/chat/rooms/components/room-item/room-item.time.tsx
@@ -3,12 +3,12 @@ import { forwardRef, useMemo } from 'react';
 import moment from 'moment';
 
 export interface RoomItemTimeProps
-  extends React.HTMLAttributes<HTMLDivElement> {
+  extends React.HTMLAttributes<HTMLSpanElement> {
   date?: string;
 }
 
-export const RoomItemTime = forwardRef<HTMLDivElement, RoomItemTimeProps>(
-  ({ date }) => {
+export const RoomItemTime = forwardRef<HTMLSpanElement, RoomItemTimeProps>(
+  ({ date, ...props }, ref) => {
     const time = useMemo(() => {
       if (date) {
         if (moment(date).isSame(moment(), 'day')) {
@@ -21,7 +21,13 @@ export const RoomItemTime = forwardRef<HTMLDivElement, RoomItemTimeProps>(
       return '';
     }, [date]);
     return (
-      <span className="ml-auto shrink-0 pl-2 text-sm font-light">{time}</span>
+      <span
+        ref={ref}
+        {...props}
+        className="ml-auto shrink-0 pl-2 text-sm font-light"
+      >
+        {time}
+      </span>
     );
   },
 );
