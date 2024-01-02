4fb1931ada92ce7a15988a3fbc7e9ba633998d8f
dudaji <sipqko@gmail.com>
2024-01-03 06:51:22 +0900

    Add prenet window and change sample as child window

--- sandbox/always-on-top-sample/main.js
+++ sandbox/always-on-top-sample/main.js
@@ -1,20 +1,31 @@
 const { app, screen, BrowserWindow } = require('electron')
 
-const createWindow = () => {
-    const mainScreen = screen.getPrimaryDisplay()
+const createMain = () => {
+    const win = new BrowserWindow({
+    })
+    win.loadFile('index.html')
+    return win
+}
 
+const createCanvas = (parent) => {
     const win = new BrowserWindow({
         transparent: true,
         frame: false,
         alwaysOnTop: true,
         show: false,
     })
+    win.setParentWindow(parent)
     win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true })
     win.setIgnoreMouseEvents(true, { forward: true })
 
-    win.loadFile('index.html')
+    win.loadFile('canvas.html')
     win.maximize()
     win.show()
+}
+
+const createWindow = () => {
+    const parent = createMain()
+    createCanvas(parent)
 }
 
 app.whenReady().then(() => {
