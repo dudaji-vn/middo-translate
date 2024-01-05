f4fe8190eb29d67e079ab338d6099df8ad6b14a6
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-01-05 16:35:35 +0700

    Add modal provider and modal store

---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,37 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/feedback';
+
+import { useModalStore } from '@/stores/modal-store';
+
+export interface ModalProviderProps {}
+
+export const ModalProvider = (props: ModalProviderProps) => {
+  const { description, hide, isShow, title } = useModalStore();
+  return (
+    <AlertDialog
+      open={isShow}
+      onOpenChange={(open) => {
+        if (!open) {
+          hide();
+        }
+      }}
+    >
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction>Close</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
