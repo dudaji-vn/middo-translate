e9151f19939a07e8d58120126efd131738ed5aef
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-01-08 16:45:45 +0700

    Update firebase-messaging-sw.js and notify-logo.png

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -16,16 +16,11 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload,
-  );
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.data.title;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: './logo.png',
+    body: payload.data.body,
+    icon: '/notify-logo.png',
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
