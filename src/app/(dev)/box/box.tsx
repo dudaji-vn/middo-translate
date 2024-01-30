d04a4de92d9b6c8388e0a25c2bc014fd6cd1b322
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-01-30 15:09:27 +0700

    Add file upload feature to media upload drop zone component and message editor text input

---
+++ src/app/(dev)/box/box.tsx
@@ -0,0 +1,6 @@
+'use client';
+export interface BoxProps {}
+
+export const Box = (props: BoxProps) => {
+  return <div>Box</div>;
+};
--- src/features/chat/messages/components/time-display.tsx
+++ src/features/chat/messages/components/time-display.tsx
@@ -1,18 +1,20 @@
+import { cn } from '@/utils/cn';
 import { formatTimeDisplay } from '../../rooms/utils';
 
 export interface LimeDisplayProps {
   time?: string;
+  className?: string;
 }
 
-export const TimeDisplay = (props: LimeDisplayProps) => {
-  if (!props.time) return null;
+export const TimeDisplay = ({ className, time }: LimeDisplayProps) => {
+  if (!time) return null;
   return (
-    <div className="my-2 flex items-center justify-center">
-      <div className="flex items-center space-x-2">
+    <div className={cn('my-2 flex items-center justify-center', className)}>
+      <div className="flex h-12 items-center justify-center space-x-2">
         <div className="bg-primary/30 h-[1px] w-16" />
-        <div className="text-sm font-light text-neutral-300">
-          {formatTimeDisplay(props.time)}
-        </div>
+        <span className="text-sm  font-light text-neutral-300">
+          {formatTimeDisplay(time)}
+        </span>
         <div className="bg-primary/30 h-[1px] w-16" />
       </div>
     </div>
