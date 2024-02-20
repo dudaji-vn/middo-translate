72648c46239af36dbdffbd48d00c4dba0347b127
huyenkvg <huyenkvg@gmail.com>
2024-02-20 15:01:46 +0700

    feat: update useKeyboardShortcut to prevent overriding text-editor

--- src/hooks/use-keyboard-shortcuts.ts
+++ src/hooks/use-keyboard-shortcuts.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTranslateStore } from "@/stores/translate.store";
 import { useEffect } from "react";
 
 type Key = "shift" | string;
@@ -8,17 +9,20 @@ export const useKeyboardShortcut = (
   keys: Key[],
   callback: () => void
 ) => {
+  const {
+    isFocused
+  } = useTranslateStore();
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      console.log('keys', event.key)
-      event.preventDefault();
-      if (
+      if (!isFocused &&
         keys.every(
           (key) =>
             (key === "shift" && event.shiftKey) ||
             (typeof key === "string" && event.key.toLowerCase() === key.toLowerCase())
         )
       ) {
+
         callback();
       }
     };
