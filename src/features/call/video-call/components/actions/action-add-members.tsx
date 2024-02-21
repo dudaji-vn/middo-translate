8b6361423ec65ef48f3312420797bb9717762d48
huyenkvg <huyenkvg@gmail.com>
2024-02-21 09:29:23 +0700

    feat: add shortcut for openning modal add user

--- src/features/call/video-call/components/actions/action-add-members.tsx
+++ src/features/call/video-call/components/actions/action-add-members.tsx
@@ -2,11 +2,18 @@ import { Button } from '@/components/actions';
 import Tooltip from '@/components/data-display/custom-tooltip/tooltip';
 import { CALL_TYPE } from '@/features/call/constant/call-type';
 import { useVideoCallStore } from '@/features/call/store/video-call.store';
+import { useKeyboardShortcut } from '@/hooks/use-keyboard-shortcuts';
 import { UserPlus2 } from 'lucide-react';
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
+import { boolean } from 'yup';
 
+const SHORTCUT_ADD_MEMBERS = ['a'];
 const ActionAddMembers = () => {
-  const { room, isFullScreen, setModalAddUser } = useVideoCallStore();
+  const { room, isFullScreen, setModalAddUser, isShowModalAddUser } = useVideoCallStore();
+
+  useKeyboardShortcut(SHORTCUT_ADD_MEMBERS, () => {
+    setModalAddUser(!isShowModalAddUser);
+  });
 
   return (
     <Tooltip
