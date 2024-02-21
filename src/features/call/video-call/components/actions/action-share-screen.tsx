380a550e4f865df4f26025a0ffde7c16a0d8d06b
huyenkvg <huyenkvg@gmail.com>
2024-02-21 09:47:33 +0700

    feat: add shortcut for middo call sharing screen

--- src/features/call/video-call/components/actions/action-share-screen.tsx
+++ src/features/call/video-call/components/actions/action-share-screen.tsx
@@ -5,11 +5,14 @@ import { MonitorUp } from 'lucide-react';
 import React, { memo } from 'react';
 import useHaveShareScreen from '../../hooks/use-have-share-screen';
 import Tooltip from '@/components/data-display/custom-tooltip/tooltip';
+import { useKeyboardShortcut } from '@/hooks/use-keyboard-shortcuts';
 
+const SHORTCUT_SHARING_SCREEN = ['s'];
 const ActionShareScreen = () => {
   const { isShareScreen } = useMyVideoCallStore();
   const { handleShareScreen } = useVideoCallContext();
   const haveShareScreen = useHaveShareScreen();
+  useKeyboardShortcut(SHORTCUT_SHARING_SCREEN, handleShareScreen);
   return (
     <Tooltip
       title={isShareScreen ? 'Stop sharing screen' : 'Share screen'}
