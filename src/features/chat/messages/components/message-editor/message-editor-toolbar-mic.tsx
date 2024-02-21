cdb0e1e005c700565ba41bb36dc8892e38ef1910
huyenkvg <huyenkvg@gmail.com>
2024-02-21 09:03:57 +0700

    feat: speech-to-text shortcut

--- src/features/chat/messages/components/message-editor/message-editor-toolbar-mic.tsx
+++ src/features/chat/messages/components/message-editor/message-editor-toolbar-mic.tsx
@@ -2,15 +2,33 @@ import { Button } from '@/components/actions';
 import { Mic } from 'lucide-react';
 import { useChatStore } from '@/features/chat/store';
 import { useMessageEditorText } from './message-editor-text-context';
+import { useKeyboardShortcut } from '@/hooks/use-keyboard-shortcuts';
+import { on } from 'events';
+import { useCallback } from 'react';
 
 export interface MessageEditorToolbarMicProps {}
 
+const SHORTCUT_SPEECH_TO_TEXT = ['shift', ' '];
 export const MessageEditorToolbarMic = (
   props: MessageEditorToolbarMicProps,
 ) => {
   const { listening, handleStopListening, handleStartListening, userLanguage } =
     useMessageEditorText();
   const { setSrcLang, srcLang } = useChatStore((s) => s);
+
+  const onStartListening = useCallback(() => {
+    if (srcLang === 'auto') {
+      setSrcLang(userLanguage);
+      handleStartListening(userLanguage);
+    } else {
+      handleStartListening();
+    }
+  }, [handleStartListening, setSrcLang, srcLang, userLanguage]);
+
+  useKeyboardShortcut(SHORTCUT_SPEECH_TO_TEXT, (e) =>{
+    listening ? handleStopListening() : onStartListening();
+    e.preventDefault();}
+  );
 
   return (
     <>
@@ -25,14 +43,7 @@ export const MessageEditorToolbarMic = (
         </Button.Icon>
       ) : (
         <Button.Icon
-          onClick={() => {
-            if (srcLang === 'auto') {
-              setSrcLang(userLanguage);
-              handleStartListening(userLanguage);
-            } else {
-              handleStartListening();
-            }
-          }}
+          onClick={onStartListening}
           variant="ghost"
           size="xs"
           color="default"
