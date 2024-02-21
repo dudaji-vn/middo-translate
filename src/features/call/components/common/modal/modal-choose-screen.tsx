0614ea6407d7fe960770038a454bd079259f9568
VinhTieng <vinhtieng123@gmail.com>
2024-02-21 09:51:00 +0700

    Add screen sharing functionality

--- src/features/call/components/common/modal/modal-choose-screen.tsx
+++ src/features/call/components/common/modal/modal-choose-screen.tsx
@@ -9,12 +9,28 @@ import {
 } from '@/components/feedback';
 import { useVideoCallStore } from '../../../store/video-call.store';
 import { useParticipantVideoCallStore } from '../../../store/participant.store';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
-
+import { cn } from '@/utils/cn';
+import socket from '@/lib/socket-io';
+import { useAuthStore } from '@/stores/auth.store';
+import { SOCKET_CONFIG } from '@/configs/socket';
+import { useMyVideoCallStore } from '@/features/call/store/me.store';
+import toast from 'react-hot-toast';
+interface MediaSource {
+    id: string;
+    thumbnail: string;
+    name: string;
+}
 export const ModalChooseScreen = () => {
     const { showChooseScreen, setChooseScreen } = useVideoCallStore();
-    const [sources, setSources] = useState([]);
+    const [selectedSource, setSelectedSource] = useState<MediaSource>();
+    const [sources, setSources] = useState<MediaSource[]>([]);
+    const { user } = useAuthStore();
+    const { addParticipant } = useParticipantVideoCallStore();
+    const { setShareScreen, setShareScreenStream} = useMyVideoCallStore();
+    const { room } = useVideoCallStore();
+
     useEffect(() => {
         //@ts-ignore
         if (!navigator?.mediaDevices?.getAllSources) return;
@@ -22,10 +38,35 @@ export const ModalChooseScreen = () => {
             //@ts-ignore
             const sources = await navigator.mediaDevices.getAllSources();
             setSources(sources);
+            console.log(sources)
         }
 
         getAllSource();
     }, []);
+
+    const handleShareScreen =useCallback(async ()=>{
+        try {
+            if (!socket.id || !selectedSource) return;
+            //@ts-ignore
+            let stream: MediaStream = await navigator.mediaDevices.getDisplayMedia(selectedSource.id)
+            const shareScreen = {
+                stream,
+                user: user,
+                isMe: true,
+                isShareScreen: true,
+                socketId: socket.id,
+            };
+            addParticipant(shareScreen);
+            setShareScreen(true);
+            setShareScreenStream(stream);
+            socket.emit(SOCKET_CONFIG.EVENTS.CALL.SHARE_SCREEN, room?._id);
+        } catch (err: unknown) {
+            if (err instanceof Error && err.name !== 'NotAllowedError') {
+                toast.error('Device not supported for sharing screen');
+            }
+        }
+        setChooseScreen(false)
+    }, [addParticipant, room?._id, selectedSource, setChooseScreen, setShareScreen, setShareScreenStream, user])
 
     return (
         <AlertDialog open={showChooseScreen} onOpenChange={() => setChooseScreen(false)}>
@@ -37,7 +78,9 @@ export const ModalChooseScreen = () => {
                     <div className='flex gap-x-1 gap-y-3 justify-around flex-wrap'>
                         {sources.map((source: any) => {
                             return (
-                                <div key={source.id} className='w-[120px]'>
+                                <div key={source.id} 
+                                    className={cn('w-[120px]', selectedSource?.id === source.id ? 'border-2 border-primary' : 'border-2 border-transparent')}
+                                    onClick={()=>setSelectedSource(source)}>
                                     <h4 className='truncate'>{source.name}</h4>
                                     <div className='w-full'>
                                         <Image 
@@ -57,6 +100,8 @@ export const ModalChooseScreen = () => {
                     <AlertDialogAction
                         type="submit"
                         className="bg-primary text-background active:!bg-primary-darker md:hover:bg-primary-lighter"
+                        disabled={!selectedSource}
+                        onClick={handleShareScreen}
                     >
                         Share
                     </AlertDialogAction>
