b2eb8a0a7582243d2561f36645dddf6bff114449
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-03-05 14:31:29 +0700

    Fix turn off speaker

--- src/hooks/use-text-to-speech.ts
+++ src/hooks/use-text-to-speech.ts
@@ -1,6 +1,7 @@
 import { SUPPORTED_VOICE_MAP } from '@/configs/default-language';
 import { textToSpeech } from '@/services/voices.service';
-import { useState } from 'react';
+import { useAppStore } from '@/stores/app.store';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useTextToSpeech = (
   languageCode?: string,
@@ -8,6 +9,38 @@ export const useTextToSpeech = (
 ): { speak: (text?: string) => void; stop: () => void; isPlaying: boolean } => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const { currentAudio, setCurrentAudio } = useAppStore((state) => {
+    return {
+      currentAudio: state.currentAudio,
+      setCurrentAudio: state.setCurrentAudio,
+    };
+  });
+
+  useEffect(() => {
+    const handleAudioPause = () => {
+      setIsPlaying(false);
+    };
+
+    if (audio) {
+      audio.addEventListener('pause', handleAudioPause);
+    }
+
+    return () => {
+      if (audio) {
+        audio.pause();
+        audio.removeEventListener('pause', handleAudioPause);
+      }
+    };
+  }, [audio]);
+
+  const stop = useCallback(() => {
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+      setIsPlaying(false);
+      setAudio(null);
+    }
+  }, [audio]);
 
   const speak = (text?: string) => {
     if (!languageCode) return console.error('Language code is required');
@@ -25,23 +58,26 @@ export const useTextToSpeech = (
   const playAudio = async (bufferData: number[]) => {
     const blob = new Blob([new Uint8Array(bufferData)], { type: 'audio/mp3' });
     const url = URL.createObjectURL(blob);
-    const audio = new Audio(url);
-    setAudio(audio);
-    audio.play();
-    audio.addEventListener('ended', () => {
+    const newAudio = new Audio(url);
+    newAudio.addEventListener('ended', () => {
       setIsPlaying(false);
       setAudio(null);
     });
-  };
 
-  const stop = () => {
-    if (audio) {
-      console.log('stop');
-      audio.pause();
-      audio.currentTime = 0;
-      setIsPlaying(false);
-      setAudio(null);
+    // Pause current audio if it's playing
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
     }
+
+    // Set the new audio as the current audio
+    setCurrentAudio(newAudio);
+
+    // Play the new audio
+    newAudio.play();
+
+    // Set the new audio to state
+    setAudio(newAudio);
   };
 
   return { speak, stop, isPlaying };
