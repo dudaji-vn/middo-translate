c9fa53d932c8c6602be54164eb756fcf201e7984
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-03-06 10:14:39 +0700

    Add useState import and comment out unused code in HomeTemplate

--- src/app/(main-layout)/template.tsx
+++ src/app/(main-layout)/template.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { ReactNode, useCallback } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 import { cn } from '@/utils/cn';
 import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import History from './_components/history/history';
-import Phrase from './_components/phrase/phrase';
-
-export type TTranslationTab = 'history' | 'phrases';
+// import Phrases from './_components/phrases/phrases';
+// import { TranslationTab } from '@/types/translationstab.type';
 
 const HomeTemplate = ({ children }: { children: ReactNode }) => {
   // const searchParams = useSearchParams();
@@ -23,39 +22,39 @@ const HomeTemplate = ({ children }: { children: ReactNode }) => {
   //   replace(`${pathname}?${params.toString()}`);
   // }, [searchParams, replace, pathname]);
 
-  if (!isValidTab) {
-    return <main className={'h-full'}>{children}</main>;
-  }
-  return (
-    <main className={'h-full '}>
-      {/* <div
-        className={cn(
-          'flex w-full flex-col divide-x max-md:gap-6 md:h-main-container-height md:flex-row',
-        )}
-      > */}
-      <div className={cn('h-fit w-full md:w-3/4')}>{children}</div>
-      {/* <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ type: 'spring', duration: 0.6 }}
-            className={
-              'max-h  z-50 w-full border-l bg-background  md:relative md:z-auto md:h-auto md:w-1/4'
-            }
-          >
-            <Phrase
-              isSelected={currentTab === 'phrases'}
-              // onClose={onCloseTab}
-            />
-            <History
-            // isSelected={currentTab === 'history'}
-            // onClose={onCloseTab}
-            />
-          </motion.div>
-        </AnimatePresence> */}
-      {/* </div> */}
-    </main>
-  );
+  // if (!isValidTab) {
+  return <main className={'h-full'}>{children}</main>;
+  // }
+  // return (
+  //   <main className={'h-full '}>
+  //     {/* <div
+  //       className={cn(
+  //         'flex w-full flex-col divide-x max-md:gap-6 md:h-main-container-height md:flex-row',
+  //       )}
+  //     > */}
+  //     <div className={cn('h-fit w-full md:w-3/4')}>{children}</div>
+  //     {/* <AnimatePresence>
+  //         <motion.div
+  //           initial={{ opacity: 0, x: 100 }}
+  //           animate={{ opacity: 1, x: 0 }}
+  //           transition={{ type: 'spring', duration: 0.6 }}
+  //           className={
+  //             'max-h  z-50 w-full border-l bg-background  md:relative md:z-auto md:h-auto md:w-1/3 xl:w-1/4'
+  //           }
+  //         >
+  //           <Phrase
+  //             isSelected={currentTab === 'phrases'}
+  //             // onClose={onCloseTab}
+  //           />
+  //           <History
+  //           // isSelected={currentTab === 'history'}
+  //           // onClose={onCloseTab}
+  //           />
+  //         </motion.div>
+  //       </AnimatePresence> */}
+  //     {/* </div> */}
+  //   </main>
+  // );
 };
 
 export default HomeTemplate;
