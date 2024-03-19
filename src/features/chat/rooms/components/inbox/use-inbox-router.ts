03d8bdf45a7eab92ee91f90380ae257c7f87a1fb
huyenkvg <huyenkvg@gmail.com>
2024-03-19 10:10:06 +0700

    fix: prevent push to chat when on business

--- src/features/chat/rooms/components/inbox/use-inbox-router.ts
+++ src/features/chat/rooms/components/inbox/use-inbox-router.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Room } from '../../types';
 import { useParams, useRouter } from 'next/navigation';
 import { ROUTE_NAMES } from '@/configs/route-name';
+import { useBusiness } from '@/hooks/use-business';
 
 type UseInboxRouter = {
   rooms: Room[];
@@ -9,9 +10,10 @@ type UseInboxRouter = {
 export const useInboxRouter = ({ rooms }: UseInboxRouter) => {
   const router = useRouter();
   const params = useParams();
+  const { isBusiness } = useBusiness();
   useEffect(() => {
-    if (!params?.id && rooms.length > 0) {
-      router.replace(`${ROUTE_NAMES.ONLINE_CONVERSATION}/${rooms[0]._id}`);
+    if (!params?.id && rooms.length > 0 && !isBusiness) {
+      router.push(`${ROUTE_NAMES.ONLINE_CONVERSATION}/${rooms[0]._id}`);
     }
   }, [rooms, params?.id, router]);
 };
