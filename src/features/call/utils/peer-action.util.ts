b45e77c0e38bd1c359b02a4bb9b46d38f5eec438
huyenkvg <huyenkvg@gmail.com>
2024-03-29 16:54:32 +0700

    update chart UI and fix avrage response time

--- src/features/call/utils/peer-action.util.ts
+++ src/features/call/utils/peer-action.util.ts
@@ -1,21 +1,33 @@
-import Peer, { SignalData } from "simple-peer";
+import moment from 'moment';
+import Peer, { SignalData } from 'simple-peer';
 
 const peerConfigurations = {
-    iceServers: [
-        {
-            urls: process.env.NEXT_PUBLIC_TURN_SERVER_URL || "stun:stun.l.google.com:19302",
-            username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME || "",
-            credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIALS || "",
-        }
-    ],
+  iceServers: [
+    {
+      urls:
+        process.env.NEXT_PUBLIC_TURN_SERVER_URL ||
+        'stun:stun.l.google.com:19302',
+      username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME || '',
+      credential: process.env.NEXT_PUBLIC_TURN_SERVER_CREDENTIALS || '',
+    },
+  ],
 };
 
 export const createPeer = () => {
-    const peer = new Peer({ initiator: true, trickle: false, config: peerConfigurations });
-    return peer;
+  const peer = new Peer({
+    initiator: true,
+    trickle: false,
+    config: peerConfigurations,
+  });
+  return peer;
 };
-export const addPeer = (signal : SignalData) => {
-    const peer = new Peer({ initiator: false, trickle: true, config: peerConfigurations })
-    peer.signal(signal);
-    return peer;
+export const addPeer = (signal: SignalData) => {
+  const peer = new Peer({
+    initiator: false,
+    trickle: true,
+    config: peerConfigurations,
+  });
+  peer.signal(signal);
+  return peer;
 };
+
