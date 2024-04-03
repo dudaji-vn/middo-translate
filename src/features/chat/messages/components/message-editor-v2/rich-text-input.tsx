b031e6d1d974ca3f45d8798fe1cd5a69192eae3e
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-04-03 14:36:03 +0700

    Fix editor on ios

--- src/features/chat/messages/components/message-editor-v2/rich-text-input.tsx
+++ src/features/chat/messages/components/message-editor-v2/rich-text-input.tsx
@@ -3,12 +3,15 @@ import {
   MentionSuggestion,
   mentionSuggestionOptions,
 } from '@/components/mention-suggestion-options';
+import { usePlatformStore } from '@/features/platform/stores';
+import { useAppStore } from '@/stores/app.store';
 import Emoji, { gitHubEmojis } from '@tiptap-pro/extension-emoji';
 import Link from '@tiptap/extension-link';
 import Mention from '@tiptap/extension-mention';
 import Placeholder from '@tiptap/extension-placeholder';
 import { Editor, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 let typingTimeout: any = null;
@@ -27,7 +30,7 @@ export interface RichTextInputProps {
   onStoppedTyping?: (isTyping: boolean) => void;
 }
 
-export const RichTextInput = ({
+const RichTextInput = ({
   className,
   onClipboardEvent,
   onCreated,
@@ -42,6 +45,7 @@ export const RichTextInput = ({
   suggestions = [],
 }: RichTextInputProps) => {
   const { t } = useTranslation('common');
+  const isMobile = usePlatformStore((state) => state.platform === 'mobile');
   const editor = useEditor({
     editorProps: {
       transformPastedHTML(html) {
@@ -101,11 +105,16 @@ export const RichTextInput = ({
         },
         suggestion: mentionSuggestionOptions(suggestions ?? []),
       }),
-      EnterToSubmit.configure({
-        onSubmit: () => {
-          onSubmit?.();
-        },
-      }),
+      // if not mobile, enable enter to submit
+
+      isMobile
+        ? (null as any)
+        : EnterToSubmit.configure({
+            onSubmit: () => {
+              onSubmit?.();
+            },
+          }),
+      ,
       Emoji.configure({
         enableEmoticons: true,
         emojis: gitHubEmojis,
@@ -128,3 +137,7 @@ export const RichTextInput = ({
   });
   return <EditorContent className={className} editor={editor} />;
 };
+
+const RichTextInputMemo = React.memo(RichTextInput);
+
+export { RichTextInputMemo as RichTextInput };
