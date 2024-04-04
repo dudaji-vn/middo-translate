122539e898ec3cfdfc8fc6c2792854ccde33b0b8
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-04-04 13:43:18 +0700

    Update fetch-commit.sh and add VersionModal component

--- fetch-commit.sh
+++ fetch-commit.sh
@@ -1,10 +1,7 @@
-
-latest_commit=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate/commits?sha=main | jq -r '.[0].sha')
+set -e 
+latest_commit=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate/commits?sha=$1 | jq -r '.[0].sha')
 echo "export const COMMIT_SHA = '$latest_commit'" > src/configs/commit-data.ts
 
-latest_commit_backend=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate-backend/commits?sha=main | jq -r '.[0].sha')
-echo "export const COMMIT_SHA_BACKEND = '$latest_commit_backend'" >> src/configs/commit-data.ts
-
-latest_tag=$(curl "https://api.github.com/repos/dudaji-vn/middo-translate/tags" | jq -r '.[0].name')
+latest_tag=$(curl -s "https://api.github.com/repos/dudaji-vn/middo-translate/tags" | jq -r '.[0].name')
 echo "export const LATEST_TAG = '$latest_tag'" >> src/configs/commit-data.ts
 
