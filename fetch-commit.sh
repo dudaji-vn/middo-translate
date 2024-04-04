4abadbbb55a270b44a972a86c21dae0f3acf858e
huyenkvg <huyenkvg@gmail.com>
2024-04-04 11:38:13 +0700

    Update commit and tag information in fetch-commit.sh and header.tsx

--- fetch-commit.sh
+++ fetch-commit.sh
@@ -1,3 +1,10 @@
 
-latest_commit=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate/commits?sha=develop | jq -r '.[0].sha')
+latest_commit=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate/commits?sha=main | jq -r '.[0].sha')
 echo "export const COMMIT_SHA = '$latest_commit'" > src/configs/commit-data.ts
+
+latest_commit_backend=$(curl -s https://api.github.com/repos/dudaji-vn/middo-translate-backend/commits?sha=main | jq -r '.[0].sha')
+echo "export const COMMIT_SHA_BACKEND = '$latest_commit_backend'" >> src/configs/commit-data.ts
+
+latest_tag=$(curl "https://api.github.com/repos/dudaji-vn/middo-translate/tags" | jq -r '.[0].name')
+echo "export const LATEST_TAG = '$latest_tag'" >> src/configs/commit-data.ts
+
