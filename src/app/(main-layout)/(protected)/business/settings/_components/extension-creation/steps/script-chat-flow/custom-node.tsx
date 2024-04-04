030f0ac3cae386fe45866b7ea90dde51e7bacaba
huyenkvg <huyenkvg@gmail.com>
2024-04-04 17:34:16 +0700

    resize nested flow

--- src/app/(main-layout)/(protected)/business/settings/_components/extension-creation/steps/script-chat-flow/custom-node.tsx
+++ src/app/(main-layout)/(protected)/business/settings/_components/extension-creation/steps/script-chat-flow/custom-node.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import ReactFlow, { Background, NodeChange, EdgeChange, Connection, Edge, BackgroundVariant } from 'reactflow';
 import 'reactflow/dist/style.css';
 
@@ -12,17 +12,27 @@ import { useCallback } from 'react';
 import { Handle, Position } from 'reactflow';
 import { Node } from "reactflow";
 import { Button } from '@/components/actions';
-import { MessageSquare, MessagesSquare, Trash, Trash2 } from 'lucide-react';
+import { MessageSquare, MessagesSquare, Pen, Plus, Trash, Trash2 } from 'lucide-react';
 import { cn } from '@/utils/cn';
-import { MessageEditorTextProvider } from '@/features/chat/messages/components/message-editor/message-editor-text-context';
-import { MessageEditorToolbar } from '@/features/chat/messages/components/message-editor/message-editor-toolbar';
-import { TextInput } from '@/features/chat/messages/components/message-editor/message-editor-text-input';
 import { FlowNode } from './nested-flow';
 import { useFormContext } from 'react-hook-form';
 
 
+export type CustomNodeProps = {
+    data: FlowNode['data'];
+    isConnectable: boolean;
+    yPos: number;
+    xPos: number;
+    sourcePosition: string;
+    targetPosition: string;
+    type: string;
+    zIndex: number;
+    selected: boolean;
+    id: string;
+}
 
-function MessageNode({ data, isConnectable }: { data: any; isConnectable: boolean }) {
+
+function MessageNode({ data, isConnectable }: CustomNodeProps) {
     const onChange = useCallback((evt: any) => {
         console.log(evt.target.value);
     }, []);
@@ -59,28 +69,25 @@ function MessageNode({ data, isConnectable }: { data: any; isConnectable: boolea
         </div>
     );
 }
-function ButtonNode(node: { data: FlowNode['data']; isConnectable: boolean }) {
+function ButtonNode(node: CustomNodeProps) {
     const { data, isConnectable } = node;
     const { content, img } = data;
 
     const { watch, setValue } = useFormContext();
     const nodes = watch('nodes');
     const edges = watch('edges');
+    console.log('nodes', nodes);
+    console.log('edges', edges);
 
-
-    const onCLick = () => {
-        console.log('nodes', nodes);
-        console.log('edges', edges);
-
-    }
+    if (!node) return null;
     return (
-        <div className="button-node">
-            {/* <Handle type="target" position={Position.Left} isConnectable={isConnectable} /> */}
-            <div>   
-                <Button size={'xs'} className='h-10' onClick={onCLick}>
-                    {content}
-                </Button>
-            </div>
+        <div className="w-[300px] h-auto px-4 flex-row flex">
+            <Button size={'xs'} className='h-10 w-full' shape={'square'}>
+                {content}
+            </Button>
+            {/* <Button.Icon color={'error'} size={'xs'} variant={'ghost'} onClick={onRemoveNode}>
+                <Trash2 size={18} />
+            </Button.Icon> */}
             <Handle type="source" position={Position.Right} isConnectable={isConnectable} />
         </div>
     );
@@ -92,15 +99,62 @@ function RootNode({ data, isConnectable }: { data: FlowNode['data']; isConnectab
             <div className='p-4 w-fit  rounded-full bg-white text-primary-500-main shadow-[2px_4px_16px_2px_rgba(22,22,22,0.1)] relative'>
                 <MessagesSquare className={`w-8 h-8`} />
             </div>
-            <p className='text-sm absolute text-primary-500-main -bottom-10 left-0 -right-8'>Start conversation</p>
+            <p className='text-sm absolute text-primary-500-main -bottom-14 left-0 -right-8'>Start conversation</p>
             <Handle type="source" position={Position.Right} isConnectable={isConnectable} />
         </div>
     );
 }
+// 
 
-function ContainerNode({ data, isConnectable }: { data: any; isConnectable: boolean }) {
+function ContainerNode(node: CustomNodeProps) {
+    const { data, isConnectable } = node;
+
+    const { watch, setValue } = useFormContext();
+    const nodes = watch('nodes');
+    const edges = watch('edges');
+
+    const [width, height, childLenth] = useMemo(() => {
+        const childLength = nodes.filter((n: { parentNode: string; }) => n.parentNode === node.id).length;
+        const maxYPos = nodes.filter((n: { parentNode: string; }) => n.parentNode === node.id).reduce((acc: number, n: { position: { y: number; }; }) => {
+            return Math.max(acc, n.position.y);
+        }, 70);
+
+        return [400, 110 + maxYPos, childLength];
+    }, [nodes]);
+    const onAddNode = useCallback(() => {
+        const newButtonNode: FlowNode = {
+            id: node.id + `button${childLenth + 1}`,
+            data: { content: "New Button", },
+            position: { x: 0, y: 120 + childLenth * 50 },
+            parentNode: node.id,
+            extent: 'parent',
+            type: 'button',
+            draggable: false
+        };
+        const newEmptyMessageNode: FlowNode = {
+            id: node.id + `message${childLenth + 1}`,
+            type: 'message',
+            data: {
+                content: '',
+                img: ''
+            },
+            position: { x: node.xPos + width + 30, y: node.yPos + childLenth * 170 },
+        };
+        const newEdge: Edge = {
+            id: `${newButtonNode.id}-${newEmptyMessageNode.id}`,
+            source: newButtonNode.id,
+            target: newEmptyMessageNode.id,
+            animated: true,
+            label: newButtonNode.data.content,
+        };
+        setValue('nodes', [...nodes, newButtonNode, newEmptyMessageNode]);
+        setValue('edges', [...edges, newEdge]);
+    }, [childLenth, nodes, edges, setValue, node, width, height]);
+
     return (
-        <div className="w-[400px] h-[500px] border bg-transparent rounded-2xl">
+        <div className="w-[400px] h-[500px] border bg-transparent rounded-2xl relative"
+            style={{ width, height }}
+        >
             <Handle type="target" position={Position.Top} isConnectable={isConnectable} />
             <div className="flex flex-col gap-2 p-3">
                 <div className="flex items-center flex-row justify-between">
@@ -112,6 +166,7 @@ function ContainerNode({ data, isConnectable }: { data: any; isConnectable: bool
                         <Button.Icon color={'error'} size={'xs'} variant={'ghost'}>
                             <Trash2 size={18} />
                         </Button.Icon>
+
                     </div>
                 </div>
                 <div
@@ -119,6 +174,9 @@ function ContainerNode({ data, isConnectable }: { data: any; isConnectable: bool
                     {data.content}
                 </div>
             </div>
+            <Button.Icon className='absolute bottom-5 left-4' shape={'square'} color={'secondary'} size={'xs'} onClick={onAddNode}>
+                <Plus size={18} />
+            </Button.Icon>
         </div>
     );
 }
@@ -135,7 +193,6 @@ const nodeTypes = {
     button: ButtonNode,
     root: RootNode,
     container: ContainerNode
-
 };
 
 export {
--- src/app/(main-layout)/(protected)/business/settings/_components/extension-creation/steps/script-chat-flow/nested-flow.tsx
+++ src/app/(main-layout)/(protected)/business/settings/_components/extension-creation/steps/script-chat-flow/nested-flow.tsx
@@ -14,6 +14,9 @@ import ReactFlow, {
     BackgroundVariant,
     applyNodeChanges,
     applyEdgeChanges,
+    getIncomers,
+    getOutgoers,
+    getConnectedEdges,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { nodeTypes } from './custom-node';
@@ -22,6 +25,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Form } from '@/components/ui/form';
+import toast from 'react-hot-toast';
 
 const schemaFlow = z.object({
     nodes: z.array(z.object({
@@ -51,64 +55,67 @@ const initialNodes: FlowNode[] = [
         className: 'light',
         type: 'root'
     },
-    type: 'container',
-    position: { x: 100, y: 0 },
-    className: 'light',
-    // style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 400, height: 900 },
-  },
-  {
-    id: '4b',
-    data: { content: 'Gap quan ly' },
-    position: { x: 15, y: 125 },
-    className: 'light',
-    parentNode: '2',
-    extent: 'parent',
-    type: 'button',
-  },
-  {
-    id: '4c',
-    data: { content: 'Gap nhan vien' },
-    position: { x: 15, y: 195 },
-    className: 'light',
-    parentNode: '2',
-    extent: 'parent',
-    type: 'button',
-  },
-  {
-    id: '4b1',
-    data: { content: 'OK, toi se chuyen ban den quan ly' },
-    position: { x: 305, y: 125 },
-    className: 'light',
-    parentNode: '4b',
-    type: 'message',
-  },
-  {
-    id: '4c1',
-    data: { content: 'OK, toi se chuyen ban den nhan vien' },
-    position: { x: 305, y: 195 },
-    className: 'light',
-    parentNode: '4c',
-    type: 'message',
-  },
+    {
+        id: '2',
+        data: {
+            label: 'Actions',
+            content: "Hello, How can I help you? ",
+        },
+        type: 'container',
+        position: { x: 100, y: 0 },
+        className: 'light',
+        draggable: false,
+        // style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 400, height: 900 },
+    },
+    // {
+    //     id: '2-1',
+    //     data: { content: "Hello, How can I help you? ", },
+    //     position: { x: 0, y: 100 },
+    //     className: 'light',
+    //     parentNode: '2',
+    //     type: 'message',
+    //     extent: 'parent'
+    // },
+
+    // {
+    //     id: '4b',
+    //     data: { content: "Gap quan ly", },
+    //     position: { x: 15, y: 125 },
+    //     className: 'light',
+    //     parentNode: '2',
+    //     extent: 'parent',
+    //     type: 'button'
+    // },
+    // {
+    //     id: '4c',
+    //     data: { content: "Gap nhan vien", },
+    //     position: { x: 15, y: 195 },
+    //     className: 'light',
+    //     parentNode: '2',
+    //     extent: 'parent',
+    //     type: 'button'
+    // }, {
+    //     id: '4b1',
+    //     data: { content: "OK, toi se chuyen ban den quan ly", },
+    //     position: { x: 305, y: 125 },
+    //     className: 'light',
+    //     parentNode: '4b',
+    //     type: 'message'
+    // },
+    // {
+    //     id: '4c1',
+    //     data: { content: "OK, toi se chuyen ban den nhan vien", },
+    //     position: { x: 305, y: 195 },
+    //     className: 'light',
+    //     parentNode: '4c',
+    //     type: 'message'
+    // }
+
 ];
 
 const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2', label: 'Start conversation' },
-  {
-    id: 'e4b-4b1',
-    source: '4b',
-    target: '4b1',
-    animated: true,
-    label: 'Gap quan ly',
-  },
-  {
-    id: 'e4c-4c1',
-    source: '4c',
-    target: '4c1',
-    animated: true,
-    label: 'Gap nhan vien',
-  },
-  // { id: 'e1-4', source: '1', target: '4', animated: true },
+    { id: 'e1-2', source: '1', target: '2', label: 'Start conversation' },
+    // { id: 'e1-4', source: '1', target: '4', animated: true },
 ];
 
 const NestedFlow = () => {
@@ -131,18 +138,46 @@ const NestedFlow = () => {
     const onNodesChange = useCallback(
         (changes: NodeChange[]) => {
             // @ts-ignore
-            setValue('nodes', applyNodeChanges(changes, watchNodes));
+            // setValue('nodes', applyNodeChanges(changes, watchNodes));
         },
         [setValue, watchNodes]
     );
     const onEdgesChange = useCallback(
         (changes: EdgeChange[]) => {
             // @ts-ignore
-            setValue('edges', applyEdgeChanges(changes, watchEdges));
+            // setValue('edges', applyEdgeChanges(changes, watchEdges));
 
         },
         [setValue, watchEdges]
     );
+
+    const onNodesDelete = useCallback(
+        (nodesToDelete: Node[]) => {
+            if (nodesToDelete.some((node) => initialNodes.find(n => n.id === node.id))) {
+                toast.error('Cannot delete this step');
+                return;
+            }
+
+            const connected = getConnectedEdges(nodesToDelete, watchEdges);
+            console.log('connected', connected);
+            const newNodes = watchNodes.filter((node) => !connected.some((edge) => edge.source === node.id || edge.target === node.id));
+            console.log('newNodes', newNodes);
+            setValue('nodes', newNodes);
+        },
+        [setValue, watchNodes, watchEdges]
+    );
+
+    const onEdgeDelete = useCallback(
+        (edgesToDelete: Edge[]) => {
+            if (edgesToDelete.some((edge) => initialEdges.find(e => e.source === edge.source && e.target == edge.target))) { 
+                toast.error('Cannot delete this step');
+                return;
+            }
+            setValue('edges', watchEdges.filter((edge) => !edgesToDelete.some((e) => e.id === edge.id)));
+        },
+        [setValue, watchEdges]
+    );
+
     const onConnect = useCallback(
         (connection: Edge | Connection) => {
             // @ts-ignore
@@ -160,6 +195,8 @@ const NestedFlow = () => {
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
+                onEdgesDelete={onEdgeDelete}
+                onNodesDelete={onNodesDelete}
                 nodeTypes={nodeTypes}
                 fitView
             >
