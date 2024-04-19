9e35cba7ea825835c1c2ffc9f8b626382813e062
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-04-19 21:04:44 +0700

    Refactor CSS styles and components in various files

--- src/features/chat/components/chat-sidebar/chat-sidebar.tsx
+++ src/features/chat/components/chat-sidebar/chat-sidebar.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import { useParams, usePathname } from 'next/navigation';
-
-import ChatSidebarHeader from './chat-sidebar-header';
-import { ChatSidebarTabs } from './chat-sidebar-tabs';
-import { PropsWithChildren, ReactNode, useEffect } from 'react';
-import { ROUTE_NAMES } from '@/configs/route-name';
-import { useAppStore } from '@/stores/app.store';
-import { useBusinessNavigationData } from '@/hooks/use-business-navigation-data';
-import { useBusinessExtensionStore } from '@/stores/extension.store';
-import { TBusinessExtensionData } from '../../help-desk/api/business.service';
 import { TSpace } from '@/app/(main-layout)/(protected)/spaces/[spaceId]/_components/business-spaces';
 import { usePlatformStore } from '@/features/platform/stores';
+import { useBusinessExtensionStore } from '@/stores/extension.store';
 import { cn } from '@/utils/cn';
+import { PropsWithChildren, ReactNode, useEffect } from 'react';
+import { TBusinessExtensionData } from '../../help-desk/api/business.service';
+import ChatSidebarHeader from './chat-sidebar-header';
+import { ChatSidebarTabs } from './chat-sidebar-tabs';
 
 interface ChatSidebarProps {
   children: ReactNode;
@@ -25,16 +20,8 @@ export const ChatSidebar = ({
   children,
   spaceData,
 }: ChatSidebarProps & PropsWithChildren) => {
-  const isMobile = useAppStore((state) => state.isMobile);
   const platform = usePlatformStore((state) => state.platform);
-  const pathName = usePathname();
-  const params = useParams();
-  const { isOnBusinessChat } = useBusinessNavigationData();
   const { setBusinessExtension } = useBusinessExtensionStore();
-  const isInRoom =
-    pathName?.includes(ROUTE_NAMES.ONLINE_CONVERSATION) && !!params?.id;
-
-  const showSide = (!isMobile || !isInRoom) && (!isOnBusinessChat || !isMobile);
 
   useEffect(() => {
     if (spaceData) {
@@ -44,18 +31,16 @@ export const ChatSidebar = ({
 
   return (
     <>
-      {showSide && (
-        <div
-          className={cn(
-            'relative flex  w-full flex-col overflow-hidden border-r',
-            platform === 'mobile' ? 'h-dvh' : 'h-main-container-height',
-          )}
-        >
-          <ChatSidebarHeader />
-          {/* TODO: UPDATE THIS */}
-          <ChatSidebarTabs>{children}</ChatSidebarTabs>
-        </div>
-      )}
+      <div
+        className={cn(
+          'relative flex  w-full flex-col overflow-hidden border-r',
+          platform === 'mobile' ? 'h-dvh' : 'h-main-container-height',
+        )}
+      >
+        <ChatSidebarHeader />
+        {/* TODO: UPDATE THIS */}
+        <ChatSidebarTabs>{children}</ChatSidebarTabs>
+      </div>
     </>
   );
 };
