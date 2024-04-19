b7dbd9e12fabf20d13d67ff9fb65df0746fc34a0
Tieng <vinh.tieng@dudaji.vn>
2024-04-19 11:26:56 +0700

    Add split pane for chat layout

---
+++ src/app/(main-layout)/(protected)/talk/[id]/_component/chat-room-content.tsx
@@ -0,0 +1,63 @@
+'use client';
+import {
+  MediaUploadDropzone,
+  MediaUploadProvider,
+} from '@/components/media-upload';
+import {
+  MessageBox,
+  MessagesBoxProvider,
+} from '@/features/chat/messages/components/message-box';
+import {
+  ChatBoxFooter,
+  ChatBoxHeader,
+  RoomSide,
+} from '@/features/chat/rooms/components';
+import { PinnedBar } from '@/features/chat/rooms/components/pin-message-bar';
+import { RoomTyping } from '@/features/chat/rooms/components/room-box/room-typing';
+import { useRoomSidebarTabs } from '@/features/chat/rooms/components/room-side/room-side-tabs/room-side-tabs.hook';
+import { useAppStore } from '@/stores/app.store';
+import { Allotment } from 'allotment';
+
+const ChatRoomContent = ({ room }: { room: any }) => {
+  const isMobile = useAppStore((state) => state.isMobile);
+  const { currentSide } = useRoomSidebarTabs();
+  return (
+    <div className="flex h-full">
+      {isMobile ? (
+        <>
+          <ChatRoomMain room={room} />
+          <RoomSide />
+        </>
+      ) : (
+        <Allotment defaultSizes={[400, 300]}>
+          <Allotment.Pane minSize={250}>
+            <ChatRoomMain room={room} />
+          </Allotment.Pane>
+          {currentSide && 
+          <Allotment.Pane minSize={200} maxSize={600} preferredSize={400}>
+            <RoomSide />
+          </Allotment.Pane>}
+        </Allotment>
+      )}
+    </div>
+  );
+};
+
+const ChatRoomMain = ({ room }: { room: any }) => {
+  return (
+    <div className="flex h-full flex-1 flex-col overflow-hidden rounded-lg bg-card">
+      <ChatBoxHeader />
+      <PinnedBar />
+      <MediaUploadProvider>
+        <MediaUploadDropzone>
+          <MessagesBoxProvider room={room}>
+            <MessageBox room={room} />
+            <RoomTyping />
+            <ChatBoxFooter />
+          </MessagesBoxProvider>
+        </MediaUploadDropzone>
+      </MediaUploadProvider>
+    </div>
+  );
+};
+export default ChatRoomContent;
--- src/app/(main-layout)/(protected)/talk/layout.tsx
+++ src/app/(main-layout)/(protected)/talk/layout.tsx
@@ -1,25 +1,45 @@
+'use client';
+
 import { ChatMain } from '@/features/chat/components/chat-main';
 import { ChatSidebar } from '@/features/chat/components/chat-sidebar';
 import { FCMBackground } from '@/features/notification/components';
 import { Fragment } from 'react';
 import { Inbox } from '@/features/chat/rooms/components';
-import { Metadata } from 'next';
+import { Allotment } from 'allotment';
+import 'allotment/dist/style.css';
+
 import { FCMElectron } from '@/features/notification/components/fcm-electron';
-export const metadata: Metadata = {
-  title: 'Talk',
-};
+import { useAppStore } from '@/stores/app.store';
+
 export interface ChatLayoutProps {
   children: React.ReactNode;
 }
 
 const ChatLayout = ({ children }: ChatLayoutProps) => {
+  const isMobile = useAppStore((state) => state.isMobile);
+
   return (
     <Fragment>
-      <div className="disable-text-selection flex">
-        <ChatSidebar>
-          <Inbox />
-        </ChatSidebar>
-        <ChatMain>{children}</ChatMain>
+      <div className="disable-text-selection h-main-container-height">
+        {isMobile ? (
+          <>
+            <ChatSidebar>
+              <Inbox />
+            </ChatSidebar>
+            <ChatMain>{children}</ChatMain>
+          </>
+        ) : (
+          <Allotment defaultSizes={[300, 800]}>
+            <Allotment.Pane minSize={300} maxSize={600} preferredSize={420}>
+              <ChatSidebar>
+                <Inbox />
+              </ChatSidebar>
+            </Allotment.Pane>
+            <Allotment.Pane>
+              <ChatMain>{children}</ChatMain>
+            </Allotment.Pane>
+          </Allotment>
+        )}
       </div>
       <FCMBackground />
       <FCMElectron />
