9d1cc4607e262e0cb051ffb4d5c77f5f159f4e27
huyenkvg <huyenkvg@gmail.com>
2024-04-22 17:50:32 +0700

    Refactor import statements and components in create-extension.tsx and messages-box.context.tsx

--- src/features/chat/messages/components/message-box/messages-box.context.tsx
+++ src/features/chat/messages/components/message-box/messages-box.context.tsx
@@ -10,7 +10,6 @@ import {
 
 import { NEXT_PUBLIC_NAME } from '@/configs/env.public';
 import { SOCKET_CONFIG } from '@/configs/socket';
-import { anounymousMesssagesAPI } from '@/features/chat/help-desk/api/anonymous-message.service';
 import { Message, PinMessage } from '@/features/chat/messages/types';
 import { useGetPinnedMessages } from '@/features/chat/rooms/hooks/use-get-pinned-messages';
 import { Room } from '@/features/chat/rooms/types';
@@ -23,6 +22,7 @@ import { useParams } from 'next/navigation';
 import { roomApi } from '../../../rooms/api';
 import { useHasFocus } from '../../../rooms/hooks/use-has-focus';
 import { MessageActions } from '../message-actions';
+import { anounymousMessagesAPI } from '@/features/chat/help-desk/api/anonymous-message.service';
 
 interface MessagesBoxContextProps {
   room: Room;
