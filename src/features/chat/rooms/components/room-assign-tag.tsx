0b29255738e921a21d9460ad0bf9e1d32c410876
huyenkvg <huyenkvg@gmail.com>
2024-04-24 00:37:30 +0700

    Refactor import statements and components in room-actions.tsx, room-assign-tag.tsx, room-item-action-wrapper.tsx, and i18n language files

--- src/features/chat/rooms/components/room-assign-tag.tsx
+++ src/features/chat/rooms/components/room-assign-tag.tsx
@@ -16,6 +16,7 @@ import { TConversationTag } from '@/app/(main-layout)/(protected)/spaces/[spaceI
 import { roomApi } from '../api';
 import toast from 'react-hot-toast';
 import { ActionItem } from './room-actions';
+import { CreateOrEditTag } from '@/app/(main-layout)/(protected)/spaces/[spaceId]/settings/_components/tags-list/create-or-edit-tag';
 
 const RoomAssignTag = ({
   id,
@@ -27,11 +28,11 @@ const RoomAssignTag = ({
   onAction: () => void;
 }) => {
   const [open, setOpen] = React.useState(false);
-  const { space } = useSpaceStore();
+  const [openAddTag, setOpenAddTag] = React.useState(false);
+  const { space, setSpace } = useSpaceStore();
 
   const tags = space?.tags || ([] as TConversationTag[]);
   const { t } = useTranslation('common');
-
 
   const onUpdateRoomTag = async (tag: TConversationTag) => {
     console.log('onUpdateRoomTag', id);
@@ -45,70 +46,84 @@ const RoomAssignTag = ({
   };
 
   return (
-    <Popover open={open}>
-      <PopoverTrigger asChild>
-        <div
-          onClick={() => {
-            onAction();
-            setOpen(true);
-          }}
-          className="relative flex cursor-pointer select-none items-center gap-2 rounded-md px-3 py-1.5 text-sm outline-none transition-colors hover:bg-primary-100 focus:bg-accent focus:text-accent-foreground"
+    <>
+      <Popover open={open}>
+        <PopoverTrigger asChild>
+          <div
+            onClick={() => {
+              onAction();
+              setOpen(true);
+            }}
+            className="relative flex cursor-pointer select-none items-center gap-2 rounded-md px-3 py-1.5 text-sm outline-none transition-colors hover:bg-primary-100 focus:bg-accent focus:text-accent-foreground"
+          >
+            <Tag size={16} />
+            <span>{t(`CONVERSATION.TAG`)}</span>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent
+          align="start"
+          onMouseLeave={() => setOpen(false)}
+          className="flex h-auto flex-col  bg-white px-0 py-4"
         >
-          <Tag size={16} />
-          <span>{t(`CONVERSATION.TAG`)}</span>
-        </div>
-      </PopoverTrigger>
-      <PopoverContent
-        align="start"
-        onMouseLeave={() => setOpen(false)}
-        className="flex h-auto flex-col  bg-white px-0 py-4"
-      >
-        <div className="divide-y divide-neutral-100">
-          <div className="flex max-h-60 w-full flex-col overflow-y-auto">
-            {tags.map(({ _id, color, name }) => {
-              const isCurrent = false;
-              return (
-                <div
-                  key={name}
-                  className={cn(
-                    'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
-                    { 'bg-primary-100': isCurrent },
-                  )}
-                  onClick={() => onUpdateRoomTag({ _id, color, name })}
-                >
-                  {isCurrent ? (
-                    <CircleCheck
-                      size={18}
-                      className="border-none fill-primary-500-main stroke-white"
-                    />
-                  ) : (
-                    <Circle size={16} className="stroke-neutral-50" />
-                  )}
-                  <Circle size={12} fill={color} stroke={color} />
-                  <span className="text-base text-neutral-700">{name}</span>
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex flex-col  items-start gap-1">
-            <div
-              className={cn(
-                'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
-              )}
-            >
-              Add new tag
+          <div className="divide-y divide-neutral-100">
+            <div className="flex max-h-60 w-full flex-col overflow-y-auto">
+              {tags.map(({ _id, color, name }) => {
+                const isCurrent = false;
+                return (
+                  <div
+                    key={name}
+                    className={cn(
+                      'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
+                      { 'bg-primary-100': isCurrent },
+                    )}
+                    onClick={() => onUpdateRoomTag({ _id, color, name })}
+                  >
+                    {isCurrent ? (
+                      <CircleCheck
+                        size={18}
+                        className="border-none fill-primary-500-main stroke-white"
+                      />
+                    ) : (
+                      <Circle size={16} className="stroke-neutral-50" />
+                    )}
+                    <Circle size={12} fill={color} stroke={color} />
+                    <span className="text-base text-neutral-700">{name}</span>
+                  </div>
+                );
+              })}
             </div>
-            <div
-              className={cn(
-                'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
-              )}
-            >
-              Tags management
+            <div className="flex flex-col  items-start gap-1">
+              <div
+                className={cn(
+                  'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
+                )}
+                onClick={() => setOpenAddTag(true)}
+              >
+                Add new tag
+              </div>
+              <div
+                className={cn(
+                  'flex w-full min-w-fit  cursor-pointer flex-row items-center justify-stretch gap-3 px-4 py-2 hover:bg-neutral-100',
+                )}
+              >
+                Tags management
+              </div>
             </div>
           </div>
-        </div>
-      </PopoverContent>
-    </Popover>
+        </PopoverContent>
+      </Popover>
+      {space && openAddTag && (
+        <CreateOrEditTag
+          spaceId={space._id}
+          tags={tags}
+          open={openAddTag}
+          onCreateSuccess={(tags) => {
+            setSpace({ ...space, tags });
+          }}
+          onOpenChange={(open) => setOpenAddTag(open)}
+        />
+      )}
+    </>
   );
 };
 
