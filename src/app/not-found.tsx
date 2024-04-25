12a8c7400622657c6964931d3601744bdcf1c18d
Tieng <vinh.tieng@dudaji.vn>
2024-04-25 14:35:53 +0700

    Add 404 page with translations

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { Typography } from "@/components/data-display";
 import { MainLayout } from "@/components/layout/main-layout"
 import Image from "next/image"
+import { useTranslation } from "react-i18next";
 
 function NotFoundPage() {
+    const {t} = useTranslation('common')
 	return <MainLayout>
-        <div className="w-full mx-auto p-4 h-main-container-height flex items-center justify-center">
+        <div className="w-full mx-auto p-4 h-main-container-height flex flex-col items-center justify-center">
             <div>
                 <Image 
                     src="/404.svg" 
@@ -14,6 +17,12 @@ function NotFoundPage() {
                     height={400} 
                 />
             </div>
+            <Typography variant={"h1"} className="text-3xl mt-10 text-neutral-400 text-center">
+                {t('404.TITLE')}
+            </Typography>
+            <Typography variant={"h6"} className=" font-normal mt-3 text-neutral-600 text-center">
+            {t('404.DESCRIPTION')}
+            </Typography>
         </div>
   </MainLayout>
 }
