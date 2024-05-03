be0b2c376d96b06e85933f66dd7bbd64bf0a461e
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-05-03 09:19:51 +0700

    Improve translation

--- src/app/api/languages/v3/detect/route.ts
+++ src/app/api/languages/v3/detect/route.ts
@@ -1,7 +1,8 @@
-import { translate } from '@/lib/cloud-translate';
+import { DUDAJI_API_URL } from '@/configs/env.public';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
+  console.log('Detect Language');
   try {
     const body = await request.json();
     const text = body.content;
@@ -10,9 +11,23 @@ export async function POST(request: Request) {
         data: '',
       });
     }
-    const [response] = await translate.detect(text);
+
+    const url = `${DUDAJI_API_URL}/api/v1/language/detection`;
+    const headers = {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+    const response = await fetch(url, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ text }),
+    });
+
+    const result = await response.json();
     return Response.json({
-      data: response,
+      data: {
+        language: result.detectedSourceLanguage,
+      },
     });
   } catch (error: any) {
     return NextResponse.next({
