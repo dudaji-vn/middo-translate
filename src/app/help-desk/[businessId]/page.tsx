771c80499f323bece88f078cb5bbc3fbb3d85457
huyenkvg <huyenkvg@gmail.com>
2024-05-08 17:47:51 +0700

    Fix: start conversation component to include visitor data for user data verification

--- src/app/help-desk/[businessId]/page.tsx
+++ src/app/help-desk/[businessId]/page.tsx
@@ -6,11 +6,15 @@ import StartAConversation from './[...slugs]/_components/start-conversation/star
 
 const HelpDeskStartConversationPage = async ({
   params: { slugs, businessId },
+  searchParams: { originReferrer },
   ...props
 }: {
   params: {
     businessId: string;
     slugs: string[];
+  };
+  searchParams: {
+    originReferrer: string;
   };
 }) => {
   const extensionData = await businessAPI.getExtensionByBusinessId(businessId);
@@ -24,9 +28,10 @@ const HelpDeskStartConversationPage = async ({
     refer: referer,
     allowedDomains: extensionData.domains,
   });
-  const isRedirectedFromRatePage = referer?.startsWith(
-    `${process.env.NEXT_PUBLIC_URL}/help-desk/${businessId}/rate`,
-  );
+  const isRedirectedFromRatePage =
+    referer?.startsWith(
+      `${process.env.NEXT_PUBLIC_URL}/help-desk/${businessId}/rate`,
+    ) && originReferrer;
 
   if (!allowedDomain && !isRedirectedFromRatePage) {
     notFound();
@@ -35,7 +40,7 @@ const HelpDeskStartConversationPage = async ({
     <StartAConversation
       visitorData={JSON.stringify(headersList)}
       extensionData={extensionData}
-      fromDomain={allowedDomain}
+      fromDomain={allowedDomain || originReferrer}
     />
   );
 };
