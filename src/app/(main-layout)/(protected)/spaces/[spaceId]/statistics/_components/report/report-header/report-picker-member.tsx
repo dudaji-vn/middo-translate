9b87e6a5243aa73f8f4dc34fbba12642fd034252
huyenkvg <huyenkvg@gmail.com>
2024-05-22 14:14:30 +0700

    feat: Update ReportPickerMember component to handle member selection and update URL parameters

--- src/app/(main-layout)/(protected)/spaces/[spaceId]/statistics/_components/report/report-header/report-picker-member.tsx
+++ src/app/(main-layout)/(protected)/spaces/[spaceId]/statistics/_components/report/report-header/report-picker-member.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { User } from 'lucide-react';
-import { useParams, useRouter } from 'next/navigation';
-import React, { use, useMemo, useState } from 'react';
+import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import React, { use, useEffect, useMemo, useState } from 'react';
 import ReportDropdown, { DropdownOption } from '../../report-dropdown';
 import { useAuthStore } from '@/stores/auth.store';
+import { ROUTE_NAMES } from '@/configs/route-name';
 
 export type MemberPickerType = string;
 
@@ -23,9 +24,14 @@ const OPTION_ALL = {
 
 const ReportPickerMember = ({ ...props }: ReportPickerMemberProps) => {
   const [openDropdown, setOpenDropdown] = useState(false);
+  const searchParams = useSearchParams();
+  const current = new URLSearchParams(
+    Array.from(searchParams?.entries() || []),
+  );
+  const currentMember = searchParams?.get('member');
   const { space } = useAuthStore();
-  const [selectedMember, setSelectedMember] =
-    useState<DropdownOption>(OPTION_ALL);
+  const router = useRouter();
+  const [selectedMember, setSelectedMember] = useState<DropdownOption>();
   const options = useMemo(() => {
     return (space?.members.map((member) => ({
       name: member.email,
@@ -33,16 +39,32 @@ const ReportPickerMember = ({ ...props }: ReportPickerMemberProps) => {
     })) || []) as DropdownOption[];
   }, [space]);
 
+  const onSelectMember = (option: DropdownOption) => {
+    if (option.value) {
+      current.set('member', option.value);
+    } else {
+      current.delete('member');
+    }
+    const href = `${ROUTE_NAMES.SPACES}/${space?._id}/statistics?${current.toString()}`;
+    router.push(href);
+  };
+  useEffect(() => {
+    if (currentMember) {
+      setSelectedMember(
+        options.find((option) => option.value === currentMember),
+      );
+    } else {
+      setSelectedMember(OPTION_ALL);
+    }
+  }, [currentMember]);
+
   return (
     <>
       <ReportDropdown
         open={openDropdown}
         onOpenChange={setOpenDropdown}
         selectedOption={selectedMember}
-        onSelectChange={(option) => {
-          console.log(option);
-          setSelectedMember(option);
-        }}
+        onSelectChange={onSelectMember}
         options={[OPTION_ALL, ...options] as DropdownOption[]}
         startIcon={<User />}
       />
