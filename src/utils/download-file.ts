0ebe67702862e2dfa4c0527a13bce367367392a4
Tieng <vinh.tieng@dudaji.vn>
2024-05-24 10:33:27 +0700

    Add support for downloading multiple files at once

--- src/utils/download-file.ts
+++ src/utils/download-file.ts
@@ -1,5 +1,7 @@
 import download from "downloadjs";
-interface IDownloadFile {
+import JSZip from "jszip";
+
+export interface IDownloadFile {
     url: string;
     fileName?: string;
     mimeType: string;
@@ -14,3 +16,33 @@ export default function downloadFile({ url, fileName, mimeType, successCallback 
         if(successCallback) successCallback();
     });
 }
+
+export const downloadFiles = (files: IDownloadFile[]) => {
+    // Case: Single file
+    if(files.length === 1) {
+        downloadFile(files[0]);
+        return;
+    }
+    // Case: Multiple files
+    const zip = new JSZip();
+    const promises: Promise<any>[] = [];
+    files.forEach((file: IDownloadFile) => {
+        promises.push(
+            fetch(file.url)
+            .then(response => response.blob())
+            .then(blob => {
+                zip.file(file.fileName || 'download', blob);
+            })
+        );
+    });
+    const date = new Date();
+    const today = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    const fileName = `Middo-${today}.zip`;
+    Promise.all(promises).then(() => {
+        zip.generateAsync({ type: "blob" })
+        .then((content) => {
+            download(content, fileName, 'application/zip');
+        });
+    });
+
+};
