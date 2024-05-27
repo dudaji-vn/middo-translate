a6b5267e54789d4bb55f6bf003c665bbc418ab28
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-05-27 10:31:22 +0700

    Refactor useNotification hook to handle Electron environment

--- src/features/notification/hooks/use-notification.tsx
+++ src/features/notification/hooks/use-notification.tsx
@@ -3,20 +3,23 @@ import { useEffect, useMemo, useState } from 'react';
 import { notificationApi } from '../api';
 import { requestForToken } from '@/lib/firebase';
 import { useNotificationStore } from '../store';
+import { useElectron } from '@/hooks/use-electron';
 
 export const useNotification = () => {
   const [permission, setPermission] = useState<NotificationPermission | null>(
     null,
   );
+  const { isElectron } = useElectron();
   const [fcmToken, setFcmToken] = useState<string | null>(null);
   const { isDenied, setDenied } = useNotificationStore((state) => state);
   const [isSubscribed, setIsSubscribed] = useState<boolean>(true);
   const isShowRequestPermission = useMemo(() => {
+    if (isElectron) return false;
     if (isDenied) return false;
     if (permission === 'default') return true;
     if (permission === 'granted' && !isSubscribed) return true;
     return false;
-  }, [isDenied, isSubscribed, permission]);
+  }, [isDenied, isElectron, isSubscribed, permission]);
 
   const checkSubscription = async () => {
     try {
