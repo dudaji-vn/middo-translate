98fbdf14bdd39c974ed3479651b818d8aaeca376
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-05-29 20:32:36 +0700

    Update landing page with mobile redirect and improve lazy loading for images

--- src/components/layout/header/header-landing-page.tsx
+++ src/components/layout/header/header-landing-page.tsx
@@ -20,41 +20,43 @@ export const HeaderLandingPage = (props: Props) => {
   const isClient = useClient();
   const platform = usePlatformStore((state) => state.platform);
   const pathName = usePathname();
-  const isMobile = useAppStore(state =>state.isMobile)
+  const isMobile = useAppStore((state) => state.isMobile);
   if (!isClient) return null;
   if (platform === 'mobile') return null;
 
   const isCurrentPath = (href: string) =>
-  href == ROUTE_NAMES.ROOT
-    ? pathName === ROUTE_NAMES.ROOT
-    : pathName?.includes(href);
+    href == ROUTE_NAMES.ROOT
+      ? pathName === ROUTE_NAMES.ROOT
+      : pathName?.includes(href);
 
   return (
     <div
       className={cn(
-        'z-50 flex h-header w-full items-center justify-between gap-1 md:gap-5 border-b border-neutral-50 py-4  pl-[1vw] pr-[5vw] md:pl-[5vw] bg-primary-100 relative',
+        'relative z-50 flex h-header w-full items-center justify-between gap-1 border-b border-neutral-50 bg-primary-100  py-4 pl-[1vw] pr-[5vw] md:gap-5 md:pl-[5vw]',
       )}
     >
-      <HeaderNavLandingMobile navItems={navLandingPageItems} />
+      {isMobile && <HeaderNavLandingMobile navItems={navLandingPageItems} />}
       <Link
-        href={ ROUTE_NAMES.ROOT}
+        href={ROUTE_NAMES.ROOT}
         className={cn(
           'flex w-[60px] flex-row justify-start gap-2 divide-x-[2px] divide-neutral-900',
         )}
       >
         <Image src="/logo.png" priority alt="logo" width={500} height={500} />
       </Link>
-        <div className={cn("flex-1 z-0")}>
-          <div className="md:flex w-screen flex-row items-stretch md:gap-1 lg:gap-5 shadow-none md:!ml-0 md:w-auto md:items-center justify-center hidden">
+      <div className={cn('z-0 flex-1')}>
+        <div className="hidden w-screen flex-row items-stretch justify-center shadow-none md:!ml-0 md:flex md:w-auto md:items-center md:gap-1 lg:gap-5">
           {navLandingPageItems.map((item) => {
-              return <NavItem
-                      isActive={isCurrentPath(item.href)}
-                      key={item.name}
-                      item={item}
-                    />
-            })}
-          </div>
+            return (
+              <NavItem
+                isActive={isCurrentPath(item.href)}
+                key={item.name}
+                item={item}
+              />
+            );
+          })}
         </div>
+      </div>
       <HeaderProfile />
     </div>
   );
