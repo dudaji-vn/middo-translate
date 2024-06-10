9e2379b15e121cfd32048dbfe9ce2bea15ac100f
huyenkvg <huyenkvg@gmail.com>
2024-06-10 11:01:16 +0700

    feat: Add lazy loading for chat widget iframe and adjust dimensions

--- src/app/help-desk/[businessId]/widget-notification/page.tsx
+++ src/app/help-desk/[businessId]/widget-notification/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { MessagesSquare } from 'lucide-react';
-import React, { use, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { cn } from '@/utils/cn';
 import Ping from '@/app/(main-layout)/(protected)/spaces/[spaceId]/_components/business-spaces/ping/ping';
 import { businessAPI } from '@/features/chat/help-desk/api/business.service';
@@ -21,24 +21,24 @@ const EmbedButtonPage = ({
     businessId: string;
   };
 }) => {
-  const [anonymous, setAnonymous] = React.useState();
-  const [roomId, setRoomId] = React.useState<string>('');
-  const [visitorId, setVisitorId] = React.useState<string>('');
+  const [anonymous, setAnonymous] = useState();
+  const [roomId, setRoomId] = useState<string>('');
+  const [visitorId, setVisitorId] = useState<string>('');
+  const [idsFound, setIdsFound] = useState<boolean>(false);
   const { data: room } = useGetRoomData({ roomId, userId: visitorId });
 
-  useEffect(() => {
+  const checkTheLocalStorage = () => {
     const rId = localStorage.getItem(LSK_VISITOR_ROOM_ID);
     const vId = localStorage.getItem(LSK_VISITOR_ID);
-    console.log('rId', rId);
-    console.log('vId', vId);
+    console.log('checkTheLocalStorage', rId, vId);
     if (rId && vId) {
       setRoomId(rId);
       setVisitorId(vId);
+      setIdsFound(true); // Stop checking once IDs are found
     }
-  }, []);
+  };
 
   useEffect(() => {
-    console.log('room', room);
     if (isEmpty(room?.data)) return;
     const anonymousUser = room?.data?.participants?.find(
       (e: any) => e.status === 'anonymous',
@@ -46,13 +46,26 @@ const EmbedButtonPage = ({
     setAnonymous(anonymousUser);
   }, [room]);
 
+  useEffect(() => {
+    if (!idsFound) {
+      const intervalId = setInterval(checkTheLocalStorage, 3000);
+      return () => clearInterval(intervalId);
+    }
+  }, [idsFound]);
+
   return (
-    <div className="fixed inset-0 flex bg-transparent  h-screen w-screen flex-row items-end justify-end  ">
-      <button className="relative m-2 w-fit rounded-full bg-white p-4 shadow-[2px_4px_16px_2px_rgba(22,22,22,0.1)]">
+    <div className="fixed inset-0 flex h-screen w-screen flex-row items-end justify-end bg-transparent">
+      <button
+        onClick={() => {
+          console.log('checkTheLocalStorage');
+          checkTheLocalStorage();
+        }}
+        className="relative m-2 w-fit rounded-full bg-white p-4 shadow-[2px_4px_16px_2px_rgba(22,22,22,0.1)]"
+      >
         {anonymous && room && (
           <NewMessageCatcher room={room?.data} anonymousUser={anonymous} />
         )}
-        <MessagesSquare className={`h-8 w-8 stroke-primary-500-main`} />
+        <MessagesSquare className="h-8 w-8 stroke-primary-500-main" />
       </button>
     </div>
   );
