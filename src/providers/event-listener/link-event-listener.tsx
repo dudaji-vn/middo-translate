eff73305b4367b5aab32ab9467cf748c67aea536
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-06-20 10:54:06 +0700

    Add event listener for link clicks

---
+++ src/providers/event-listener/link-event-listener.tsx
@@ -0,0 +1,35 @@
+import { useReactNativePostMessage } from '@/hooks/use-react-native-post-message';
+import { useEffect } from 'react';
+
+export const LinkEventListener = () => {
+  const { postMessage, isMobile } = useReactNativePostMessage();
+  useEffect(() => {
+    document.addEventListener('click', (event) => {
+      if (
+        event.target instanceof HTMLAnchorElement &&
+        event.target.classList.contains('link')
+      ) {
+        if (!isMobile) return;
+        event.preventDefault();
+        const href = event.target.getAttribute('href');
+        console.log('href', href);
+        if (href) {
+          postMessage({
+            type: 'Trigger',
+            data: {
+              event: 'link',
+              payload: {
+                url: href,
+              },
+            },
+          });
+        }
+      }
+    });
+    return () => {
+      document.removeEventListener('click', () => {});
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return null;
+};
