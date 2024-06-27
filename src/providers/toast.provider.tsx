e4776955903dac6b4231c9f3078d7daaf668d80d
Tieng <vinh.tieng@dudaji.vn>
2024-06-27 08:29:27 +0700

    Fix email resend error handling and add file type validation in InputImage component

--- src/providers/toast.provider.tsx
+++ src/providers/toast.provider.tsx
@@ -9,19 +9,19 @@ export const ToastProvider = () => {
       toastOptions={{
         error: {
           className:
-            '!bg-error-100 !text-neutral-800 !border !border-error dark:!bg-error-900 dark:!border-error-400 dark:!text-neutral-50',
+            '!bg-error-100 !text-neutral-800 !border !border-error dark:!bg-error-900 dark:!border-error-400 dark:!text-neutral-50 !max-w-full',
         },
         success: {
           className:
-            '!bg-success-200 !text-neutral-800 !border !border-success-700 dark:!bg-success-900 dark:!border-success-700 dark:!text-neutral-50',
+            '!bg-success-200 !text-neutral-800 !border !border-success-700 dark:!bg-success-900 dark:!border-success-700 dark:!text-neutral-50 !max-w-full',
         },
         loading: {
           className:
-            '!bg-neutral-50 !text-neutral-800 !border !border-neutral-300 dark:!bg-neutral-900 dark:!border-neutral-600 dark:!text-neutral-50',
+            '!bg-neutral-50 !text-neutral-800 !border !border-neutral-300 dark:!bg-neutral-900 dark:!border-neutral-600 dark:!text-neutral-50 !max-w-full',
         },
         blank: {
           className:
-            '!bg-neutral-50 !text-neutral-800 !border !border-neutral-300 dark:!bg-neutral-900 dark:!border-neutral-600 dark:!text-neutral-50',
+            '!bg-neutral-50 !text-neutral-800 !border !border-neutral-300 dark:!bg-neutral-900 dark:!border-neutral-600 dark:!text-neutral-50 !max-w-full',
         },
         duration: 3000,
       }}
