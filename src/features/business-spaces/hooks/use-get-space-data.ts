0c5b7259bcb8fae65d169a9a803fd4f94a61105f
huyenkvg <huyenkvg@gmail.com>
2024-06-27 14:56:31 +0700

    chore: return logic useGetSpaceData to use axios for fetching space data

--- src/features/business-spaces/hooks/use-get-space-data.ts
+++ src/features/business-spaces/hooks/use-get-space-data.ts
@@ -2,7 +2,6 @@
 
 import { BusinessTabType } from '@/app/(main-layout)/(protected)/spaces/[spaceId]/_components/business-spaces';
 import { axios } from '@/lib/axios';
-import { getInvitationByToken } from '@/services/business-space.service';
 import { useQuery } from '@tanstack/react-query';
 
 export const GET_SPACE_DATA_KEY = 'get-space-data';
@@ -10,7 +9,21 @@ export const GET_SPACE_DATA_KEY = 'get-space-data';
 export const useGetSpaceData = ({ spaceId }: { spaceId: string }) => {
   return useQuery({
     queryKey: [GET_SPACE_DATA_KEY, { spaceId }],
-    queryFn: getInvitationByToken,
+    queryFn: async () => {
+      try {
+        const response = await axios.get(`/help-desk/spaces/${spaceId}`, {
+          params: {
+            spaceId,
+          },
+        });
+        return response.data;
+      } catch (error) {
+        console.error(
+          `Error fetching space ${spaceId} from useGetSpaceData: ${error as Error}`,
+        );
+        return {};
+      }
+    },
     enabled: true,
   });
 };
