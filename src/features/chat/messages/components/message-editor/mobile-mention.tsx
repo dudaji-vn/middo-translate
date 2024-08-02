d18a7bb98e00f1275a257b2991af059736beeb18
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-08-02 11:00:37 +0700

    Refactor mobile mention logic for better performance

--- src/features/chat/messages/components/message-editor/mobile-mention.tsx
+++ src/features/chat/messages/components/message-editor/mobile-mention.tsx
@@ -1,10 +1,10 @@
 import { Editor } from '@tiptap/react';
+import { useMemo } from 'react';
 import { MentionSuggestion } from './mention-suggestion-options';
 import { MentionItem } from './suggestion-list';
-import { useMemo } from 'react';
 
 type MobileMentionProps = {
-  editor: Editor | null;
+  editor: Editor;
   suggestions: MentionSuggestion[];
   onSelect: (suggestion: MentionSuggestion) => void;
 };
@@ -13,29 +13,38 @@ export const MobileMention = ({
   suggestions,
   onSelect,
 }: MobileMentionProps) => {
-  const isMentionTrigger = useMemo(() => {
-    if (!editor) return false;
-    const mentionState = (
-      editor.state as unknown as {
+  const { active, query, range } = useMemo(() => {
+    return (
+      editor?.state as unknown as {
         [key: string]: {
           active: boolean;
+          query: string | null;
+          range: { from: number; to: number };
         };
       }
     )['mention$'];
-    return mentionState.active;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [editor?.state]);
+
+  const filteredSuggestions = useMemo(() => {
+    if (!query) return suggestions;
+    return suggestions.filter((suggestion) =>
+      suggestion.label.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [suggestions, query]);
+
   const selectItem = (index: number) => {
-    if (!editor) return;
-    const selectedSuggestion = suggestions[index];
-    const { to } = editor.state.selection;
-    editor.commands.deleteRange({ from: to - 1, to });
+    const selectedSuggestion = filteredSuggestions[index];
     const nodeAfter = editor.view.state.selection.$to.nodeAfter;
     const overrideSpace = nodeAfter?.text?.startsWith(' ');
+    editor.commands.deleteRange({
+      from: range.from,
+      to: range.to,
+    });
     editor
       .chain()
       .focus()
-      .insertContentAt(overrideSpace ? to : to - 1, [
+      .insertContentAt(overrideSpace ? range.from + 1 : range.from, [
         {
           type: 'mention',
           attrs: {
@@ -49,30 +58,15 @@ export const MobileMention = ({
         },
       ])
       .run();
+    onSelect(selectedSuggestion);
     window?.getSelection()?.collapseToEnd();
   };
 
-  const filteredSuggestions = useMemo(() => {
-    const content = editor?.state.doc.textBetween(
-      editor.state.selection.from - 1,
-      editor.state.selection.to,
-    );
-    if (!content || content == '@') return suggestions;
-    return suggestions.filter((suggestion) =>
-      suggestion.label.toLowerCase().includes(content.toLowerCase()),
-    );
-  }, [
-    editor?.state.doc,
-    editor?.state.selection.from,
-    editor?.state.selection.to,
-    suggestions,
-  ]);
-
-  if (!isMentionTrigger) return null;
+  if (!active) return null;
   return (
     <div className="relative mb-[18px] w-full">
       <div className="absolute left-0 top-0 w-full -translate-y-full overflow-hidden rounded-xl  border shadow-1">
-        <div className="no-scrollbar flex h-52 w-full flex-1 flex-col space-y-1 overflow-y-auto bg-white dark:bg-neutral-900">
+        <div className="no-scrollbar flex max-h-52 w-full flex-1 flex-col space-y-1 overflow-y-auto bg-white dark:bg-neutral-900">
           {filteredSuggestions.map((suggestion, index) => {
             return (
               <MentionItem
